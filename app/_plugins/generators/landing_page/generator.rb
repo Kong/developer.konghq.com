8e64e512bf3bd6a0f16b0384160a54faa4369025
Michael Heap <m@michaelheap.com>
2024-06-28 20:54:32 +0100

    Landing pages being output

--- app/_plugins/generators/landing_page/generator.rb
+++ app/_plugins/generators/landing_page/generator.rb
@@ -4,8 +4,8 @@ module Jekyll
   module LandingPages
     class Generator
       def self.run(site)
-        Dir.glob(File.join(site.source, '_landing_pages/*.yaml')).each do |file|
-          Jekyll::LandingPages::Page.new(site, file)
+        Dir.glob(File.join(site.source, '_landing_pages/**/*.yaml')).each do |file|
+          site.pages << Jekyll::LandingPages::Page.new(site, file)
         end
       end
     end
--- app/_plugins/generators/landing_page/page.rb
+++ app/_plugins/generators/landing_page/page.rb
@@ -2,17 +2,33 @@
 
 module Jekyll
   module LandingPages
-    class Page
+    class Page < Jekyll::Page
       def initialize(site, file)
         @site = site
         @data = YAML.load_file(file)
-        puts "CREATING A PAGE FOR #{@data['title']} at '#{output_path(file)}'"
+
+        # Set self.ext and self.basename by extracting information from the page filename
+        process('index.md')
+
+        @dir = output_path(file)
+
+        @content = "HELLO WORLD"
+        @data = {
+          'title' => 'This is a test'
+        }
+
+        # Needed so that regeneration works for single sourced pages
+        # It must be set to the source file
+        # Also, @path MUST NOT be set, it falls back to @relative_path
+        @relative_path = file
+
+        @data['layout'] = 'default'
       end
 
       private
 
       def output_path(file)
-        file.sub(@site.source, @site.dest).sub(/\/_landing_pages\/(\w+)\.yaml$/, '/\1.html')
+        file.sub(@site.source, @site.dest).sub(/\/_landing_pages\/([\w\/]+)\.yaml$/, '/\1/')
       end
     end
   end
