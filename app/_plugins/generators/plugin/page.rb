c82113a7a873a4ad183efeb9965639b8e2f8727f
Michael Heap <m@michaelheap.com>
2024-07-03 20:10:08 +0100

    Generate Plugin pages (#6)
    * Bootstrap Kong plugins
    * Initial plugin page added

---
+++ app/_plugins/generators/plugin/page.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module KongPlugin
+    class Page < Jekyll::Page
+      def initialize(site, file)
+        @site = site
+        content = File.read(file)
+
+        # Set self.ext and self.basename by extracting information from the page filename
+        process('index.md')
+
+        @dir = output_path(file)
+
+        # Load content + frontmatter from the file
+        if content =~ Jekyll::Document::YAML_FRONT_MATTER_REGEXP
+          @content = Regexp.last_match.post_match
+          @data = SafeYAML.load(Regexp.last_match(1))
+        end
+
+        # Needed so that regeneration works for single sourced pages
+        # It must be set to the source file
+        # Also, @path MUST NOT be set, it falls back to @relative_path
+        @relative_path = file
+
+        @data['layout'] = 'plugin'
+      end
+
+      def url
+        @dir.sub(@site.dest, '')
+      end
+
+      private
+
+      def output_path(file)
+        file.sub(@site.source, @site.dest).sub(%r{/_kong_plugins/([\w/-]+)\.md$}, '/plugins/\1/')
+      end
+    end
+  end
+end
