97e90b420c3d94fc72ceeaef2c77b15c339166bb
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-07-04 12:55:45 +0200

    Add Liquify filter, takes a string as input and evaluates it using the context
    Unfortunately, Jekyll doesn't support using variables in the
    frontmatter. Using this filter in a template, will evaluate any
    variables present in the input and replace them with the corresponding
    values.

---
+++ app/_plugins/filters/liquify.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module LiquifyFilter
+    def liquify(input)
+      if input.is_a? String
+        Liquid::Template.parse(input).render(@context)
+      else
+        input
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::LiquifyFilter)
