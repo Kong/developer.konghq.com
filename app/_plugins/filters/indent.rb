5317eed25ebfb23c54fef8abb1a792bd810a7574
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-07-18 09:28:45 +0200

    Refactor indent filter, it now accepts a param indicating the number of spaces to indent the content.

--- app/_plugins/filters/indent.rb
+++ app/_plugins/filters/indent.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module IndentFilter
-  def indent(input)
+  def indent(input, spaces)
     input
       .gsub("\n</code>", '</code>')
       .split("\n")
-      .map { |l| l.prepend('   ') }
+      .map { |l| l.prepend(' ' * spaces.to_i) }
       .join("\n")
   end
 end
