7b4c74011b646bcedc141d21cc6a018edf60182c
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-07-29 17:01:20 +0200

    [Plugins] Include only `_kong_plugins/<plugin-name>/index.md in the plugins collection

---
+++ app/_plugins/generators/plugins.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Jekyll
+  class PluginPagesGenerator < Jekyll::Generator
+    priority :low
+
+    def generate(site)
+      collection = site.collections['kong_plugins']
+      collection.docs.map do |doc|
+        slug = doc.relative_path
+          .gsub(collection.relative_directory, '')
+          .gsub(doc.basename, '')
+
+        doc.data['slug'] = slug
+        doc.data['permalink'] = "/plugins/#{slug}/"
+      end
+    end
+  end
+end
