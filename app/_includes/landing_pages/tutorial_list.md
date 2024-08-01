6664037b8a341161f0469f42703ca1ff89ff9752
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-08-01 09:07:24 +0200

    [Tutorials] Implement {% tutorial_list %}

--- app/_includes/landing_pages/tutorial_list.md
+++ app/_includes/landing_pages/tutorial_list.md
@@ -1,7 +1,1 @@
-<ul>
-  {% for item in include.config %}
-    <li>
-      This will show tutorials tagged with '{{ item.topic }}'{% if item.product %} in the '{{ item.product }}' product{% endif %}
-    </li>
-  {% endfor %}
-</ul>
+{% tutorial_list include.config %}
---
+++ app/_plugins/tags/tutorial_list.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Jekyll
+  class RenderTutorialList < Liquid::Tag
+    def initialize(tag_name, param, _tokens)
+      super
+
+      @param = param.strip
+      if @param.nil? || @param.empty?
+        raise ArgumentError, "Missing param for {% tutorial_list %}"
+      end
+    end
+
+    def render(context)
+      @context = context
+      @site = context.registers[:site]
+      keys = @param.split('.')
+      config = keys.reduce(context) { |c, key| c[key] }.first
+
+      tutorials = @site.collections['tutorials'].docs.select do |t|
+        t.data['tags'].include?(config['tag']) &&
+          (!config.key?('product') || t.data['products'].include?(config['product']))
+      end
+
+      @context.environments.unshift('tutorials' => tutorials)
+
+      rendered_content = Liquid::Template.parse(template).render(@context)
+
+      @context.environments.shift
+
+      rendered_content
+    end
+
+    private
+
+    def template
+      @template ||= File.read(File.expand_path('app/_includes/components/tutorial_list.html'))
+    end
+  end
+end
+
+Liquid::Template.register_tag('tutorial_list', Jekyll::RenderTutorialList)
