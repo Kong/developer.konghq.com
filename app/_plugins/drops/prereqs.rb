eae06e7be299d5a176c3890b1b78f226c54d4b2d
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-08-09 11:37:07 +0200

    Render entities in prereqs in the same order they are declared in metadata

--- app/_plugins/drops/prereqs.rb
+++ app/_plugins/drops/prereqs.rb
@@ -21,43 +21,18 @@ module Jekyll
       end
 
       def entities?
-        services.any? || routes.any? || consumers.any? || plugins.any?
+        @prereqs.any?
       end
 
       def data
-        yaml = {
-          '_format_version' => '3.0',
-        }
-        yaml.merge!('services' => services) if services.any?
-        yaml.merge!('routes' => routes) if routes.any?
-        yaml.merge!('consumers' => consumers) if consumers.any?
-        yaml.merge!('plugins' => plugins) if plugins.any?
+        yaml = { '_format_version' => '3.0' }
+
+        @prereqs.each do |k, files|
+          entities = files.map { |f| load_yaml(find_file(folder: k, example: f)) }
+          yaml.merge!(k => entities) if entities
+        end
 
         Jekyll::Utils::HashToYAML.new(yaml).convert
-      end
-
-      def services
-        @services ||= @prereqs.fetch('services', []).map do |s|
-          load_yaml(find_file(folder: 'services', example: s))
-        end
-      end
-
-      def routes
-        @routes ||= @prereqs.fetch('routes', []).map do |r|
-          load_yaml(find_file(folder: 'routes', example: r))
-        end
-      end
-
-      def consumers
-        @consumers ||= @prereqs.fetch('consumers', []).map do |c|
-          load_yaml(find_file(folder: 'consumers', example: c))
-        end
-      end
-
-      def plugins
-        @plugins ||= @prereqs.fetch('plugins', []).map do |p|
-          load_yaml(find_file(folder: 'plugins', example: p))
-        end
       end
 
       private
