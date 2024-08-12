ca4ef653b3820ecc38765d4d705441326d7a1b4d
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-08-12 19:57:20 +0200

    Remove comment

--- app/_plugins/generators/plugins.rb
+++ app/_plugins/generators/plugins.rb
@@ -8,17 +8,5 @@ module Jekyll
       site.data['kong_plugins'] ||= {};
       Jekyll::PluginPages::Generator.run(site)
     end
-
-    # def generate(site)
-    #   collection = site.collections['kong_plugins']
-    #   collection.docs.map do |doc|
-    #     slug = doc.relative_path
-    #       .gsub("#{collection.relative_directory}/", '')
-    #       .gsub("/#{doc.basename}", '')
-
-    #     doc.data['slug'] = slug
-    #     doc.data['permalink'] = "/plugins/#{slug}/"
-    #   end
-    # end
   end
 end
