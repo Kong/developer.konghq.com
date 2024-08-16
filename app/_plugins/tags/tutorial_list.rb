c128443fa397ea89cb65f0fd3400c1708b585eb6
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-08-16 13:02:06 +0200

    Add support for `tool` to tutorial_list

--- app/_plugins/tags/tutorial_list.rb
+++ app/_plugins/tags/tutorial_list.rb
@@ -21,7 +21,8 @@ module Jekyll
 
       tutorials = @site.collections['tutorials'].docs.inject([]) do |result, t|
         match = (!config.key?('tags') || t.data.fetch('tags', []).intersect?(config['tags'])) &&
-          (!config.key?('products') || t.data.fetch('products', []).intersect?(config['products']))
+          (!config.key?('products') || t.data.fetch('products', []).intersect?(config['products'])) &&
+          (!config.key?('tools') || t.data.fetch('tools', []).intersect?(config['tools']))
 
         result << t if match
         break result if result.size == quantity
