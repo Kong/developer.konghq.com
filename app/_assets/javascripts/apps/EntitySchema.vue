5688c6289786a82f472bba4002a61168474eef9f
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-09-09 14:43:26 +0200

    Update code with latest changes to spec-renderer Unfortunately, we need to hide the header and example with css

--- app/_assets/javascripts/apps/EntitySchema.vue
+++ app/_assets/javascripts/apps/EntitySchema.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="sandbox-container">
     <main class="page-main">
-      <SpecModelNode
-        v-if="schema"
-        :schema="schema.data"
+      <SpecDocument
+        v-if="parsedDocument"
+        :allow-content-scrolling="false"
+        :base-path="basePath"
+        :current-path="currentPath"
+        :document="parsedDocument"
+        :hide-insomnia-try-it="true"
+        :hide-try-it="true"
       />
+
     </main>
   </div>
 </template>
@@ -12,7 +18,7 @@
 <script setup>
 import { ref, onMounted, watch, computed } from 'vue'
 
-import { SpecModelNode, parseSpecDocument, parsedDocument } from '@kong/spec-renderer-dev'
+import { SpecDocument, parseSpecDocument, parsedDocument } from '@kong/spec-renderer-dev'
 import ApiService from '../services/api.js'
 
 const { path, product, version } = window.entitySchema;
@@ -22,6 +28,7 @@ const specText = ref('');
 const currentPath = ref(path);
 const productId = ref(product.id);
 const productVersionId = ref(version.id);
+const basePath = ref('');
 
 const schema = computed(() => {
   if (parsedDocument.value !== undefined) {
@@ -53,3 +60,29 @@ async function fetchSpec() {
   specText.value = response.data.content
 }
 </script>
+
+<style scoped>
+:deep(.default-markdown) {
+  @apply text-secondary !important;
+}
+
+:deep(.property-type) {
+  @apply text-secondary !important;
+}
+
+:deep(code) {
+  @apply bg-code-block text-white !important;
+}
+
+:deep(.schema-example) {
+  display: none;
+}
+
+:deep(.spec-renderer-page-header) {
+  display: none;
+}
+
+:deep(.property-title-name) {
+  @apply  text-brand !important;
+}
+</style>
