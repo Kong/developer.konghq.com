32ce6b7f82fd11452c492c63b737a502f18a4bcc
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-09-12 13:13:46 +0200

    Fix underline in prereqs/cleanup and how-tos

--- app/_plugins/hooks/sections/how-to.rb
+++ app/_plugins/hooks/sections/how-to.rb
@@ -5,7 +5,7 @@ module SectionWrapper
     def build_wrapper(h2, slug, title)
         Nokogiri::HTML::DocumentFragment.parse <<-HTML
             <div class="flex flex-col gap-4 collapsible" aria-expanded="true">
-                <a aria-label="Anchor" href="##{slug}" title="#{title}" class="header-link flex items-baseline justify-between">
+                <a aria-label="Anchor" href="##{slug}" title="#{title}" class="header-link flex items-baseline justify-between hover:no-underline">
                 #{h2.to_html}
                 <span class="fa fa-chevron-down text-terciary rotate-180" aria-hidden="true"></span>
                 </a>
