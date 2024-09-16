5fd33868353b5801384b84f4a154818e5be17776
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-09-16 15:27:15 +0200

    Tidy-up colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,17 +23,16 @@ module.exports = {
     extend: {
       colors:{
         brand: 'rgb(var(--color-brand), <alpha-value>)',
-        'brand-saturated': 'rgb(var(--color-brand-saturated), <alpha-value>)'
+        'brand-saturated': 'rgb(var(--color-brand-saturated), <alpha-value>)',
+        'semantic-red-primary': 'rgb(var(--color-semantic-red-primary), <alpha-value>)'
       },
       textColor: {
         primary: 'rgb(var(--color-text-primary), <alpha-value>)',
         secondary: 'rgb(var(--color-text-secondary), <alpha-value>)',
         terciary: 'rgb(var(--color-text-terciary), <alpha-value>)',
-        'semantic-red-primary': 'rgb(var(--color-semantic-red-primary), <alpha-value>)',
       },
       borderColor: {
         primary: 'rgb(var(--color-border-primary), <alpha-value>)',
-        brand: 'rgb(var(--color-brand), <alpha-value>)'
       },
       backgroundColor: {
         primary: 'rgb(var(--color-bg-primary), <alpha-value>)',
