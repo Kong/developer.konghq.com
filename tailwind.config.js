a5c82c0692b1f2670821f2d72ddde94f317be9a7
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-09-16 16:49:32 +0200

    Add semantic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,16 @@ module.exports = {
       colors:{
         brand: 'rgb(var(--color-brand), <alpha-value>)',
         'brand-saturated': 'rgb(var(--color-brand-saturated), <alpha-value>)',
-        'semantic-red-primary': 'rgb(var(--color-semantic-red-primary), <alpha-value>)'
+        'semantic-red-primary': 'rgb(var(--color-semantic-red-primary), <alpha-value>)',
+        'semantic-red-secondary': 'rgb(var(--color-semantic-red-secondary), <alpha-value>)',
+        'semantic-yellow-primary': 'rgb(var(--color-semantic-yellow-primary), <alpha-value>)',
+        'semantic-yellow-secondary': 'rgb(var(--color-semantic-yellow-secondary), <alpha-value>)',
+        'semantic-blue-primary': 'rgb(var(--color-semantic-blue-primary), <alpha-value>)',
+        'semantic-blue-secondary': 'rgb(var(--color-semantic-blue-secondary), <alpha-value>)',
+        'semantic-green-primary': 'rgb(var(--color-semantic-green-primary), <alpha-value>)',
+        'semantic-green-secondary': 'rgb(var(--color-semantic-green-secondary), <alpha-value>)',
+        'semantic-grey-primary': 'rgb(var(--color-semantic-grey-primary), <alpha-value>)',
+        'semantic-grey-secondary': 'rgb(var(--color-semantic-grey-secondary), <alpha-value>)',
       },
       textColor: {
         primary: 'rgb(var(--color-text-primary), <alpha-value>)',
