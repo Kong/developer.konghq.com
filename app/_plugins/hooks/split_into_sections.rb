303d294ada865c3ed79fedc0ad0041823b29025b
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-09-24 10:45:10 +0200

    Don't split sections of the homepage

--- app/_plugins/hooks/split_into_sections.rb
+++ app/_plugins/hooks/split_into_sections.rb
@@ -6,6 +6,7 @@ end
 
 Jekyll::Hooks.register :pages, :post_convert do |page, payload|
   next if page.data['layout'] == 'landing_page'
+  next if page.url == '/'
 
   SectionWrapper::Base.make_for(page).process
 end
