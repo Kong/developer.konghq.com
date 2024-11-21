71ff62025332ae866a3550935fbdb716ecbe0230
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-11-21 10:59:43 +0100

    Add a tool that allows us to run Vale on Frontmatters
    * XXX: It's not intended for local use
    * It replaces the files' contents with the frontmatter so that we can then
      run vale on the files.
    * It uses reads keys from .github/styles/docs/Frontmatter.txt and
      replaces the entire line with empty strings, so that we don't get
      errors on those lines (or need to add exceptions to our dictionary).
    * Unfortunately, there's some duplication in .github/styles/docs/Frontmatter.txt and
      .github/styles/landing_pages/Components.txt that we can't avoid, due
      to Vale's limitations.

---
+++ tools/vale-frontmatter/index.js
@@ -0,0 +1,68 @@
+const fs = require("fs").promises;
+const fg = require("fast-glob");
+const matter = require("gray-matter");
+const path = require("path");
+
+async function pathExists(path) {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+async function prepareMarkdownFilesForVale() {
+  const filesParam = process.argv.slice(2)[0] || "app/**/*.md";
+  let pattern;
+
+  if (await pathExists(path.resolve("../../", filesParam))) {
+    pattern = [`${filesParam}/**/*.md`];
+  } else {
+    try {
+      pattern = JSON.parse(filesParam);
+    } catch (e) {
+      console.log("Invalid path, defaulting to app/**/*.md");
+      pattern = ["app/**/*.md"];
+    }
+  }
+  let files = await fg(pattern, { cwd: "../../" });
+
+  const frontmatterDictionary = await fs.readFile(
+    `../../.github/styles/docs/Frontmatter.txt`,
+    "utf-8"
+  );
+
+  const keys = frontmatterDictionary
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0);
+
+  for (const path of files) {
+    console.log(`Processing: ${path}`);
+    const contents = await fs.readFile(`../../${path}`, "utf-8");
+    if (contents) {
+      const parsed = matter(contents);
+      let frontmatter = parsed.matter;
+
+      for (const key of keys) {
+        const keyRegex = new RegExp(`^\\s*${key}:.*$`, "gm");
+
+        frontmatter = frontmatter.replace(keyRegex, "");
+      }
+
+      await fs.writeFile(`../../${path}`, frontmatter, "utf-8");
+    }
+    console.log("Done.");
+  }
+}
+
+if (require.main === module) {
+  try {
+    prepareMarkdownFilesForVale();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = prepareMarkdownFilesForVale;
