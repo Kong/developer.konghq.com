c31d3da4bd68951ce8158188600df0cc5b2ca728
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-11-25 11:31:26 +0100

    Use console.log to log broken links results

--- tools/broken-link-checker/lib/process-result.js
+++ tools/broken-link-checker/lib/process-result.js
@@ -2,14 +2,14 @@ export default function processResult(result) {
   const { brokenLinks, otherErrors } = result;
 
   if (otherErrors.length) {
-    console.warn(
+    console.log(
       `${otherErrors.length} links have issues but returned a status code other than 404!`
     );
     console.log(JSON.stringify(otherErrors, null, 2));
   }
 
   if (brokenLinks.length) {
-    console.error(`${brokenLinks.length} Broken links found!`);
+    console.log(`${brokenLinks.length} Broken links found!`);
     console.log(JSON.stringify(brokenLinks, null, 2));
     process.exit(1);
   }
