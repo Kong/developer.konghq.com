699920a122ddd7dd3425d2be1db0136f74037f6c
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-11-29 08:59:29 +0100

    Register  BindOncePlugin, kongponents rely on it

--- app/_assets/entrypoints/api_spec.js
+++ app/_assets/entrypoints/api_spec.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import APISpec from "~/javascripts/apps/APISpec.vue";
+import { BindOncePlugin } from "vue-bind-once";
 import "@kong/spec-renderer-dev/dist/style.css";
 
 if (document.getElementById("api-spec") !== null) {
   const app = createApp(APISpec);
+  app.use(BindOncePlugin);
   app.mount("#api-spec");
 }
