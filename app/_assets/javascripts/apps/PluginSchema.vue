b713e5016d735d60b0ec29344678fd9d3a54da3e
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-12-02 11:53:21 +0100

    [Hub] Add Vue app that renders the schema

---
+++ app/_assets/javascripts/apps/PluginSchema.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <SchemaRenderer
+        v-if="schema"
+        :schema="schema"
+        :exampleVisible="false"
+        :headerVisible="false"
+      />
+    </div>
+  </template>
+
+  <script setup>
+  import { SchemaRenderer } from '@kong/spec-renderer-dev'
+
+  const schema = window.schema;
+  </script>
