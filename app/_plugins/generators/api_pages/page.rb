b3c538f930875538123f97a27395b36f31413a03
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-12-02 09:02:05 +0100

    Generate insomnia links programatically

--- app/_plugins/generators/api_pages/page.rb
+++ app/_plugins/generators/api_pages/page.rb
@@ -5,7 +5,7 @@ require_relative './base'
 module Jekyll
   module APIPages
     class Page < Base
-      attr_reader :product
+      attr_reader :product, :version, :site
 
       def initialize(product:, version:, file:, site:, frontmatter:)
         @product = product
@@ -30,10 +30,20 @@ module Jekyll
           'breadcrumbs' => ['/api/'],
           'version' => @version,
           'versions_dropdown' => Drops::OAS::VersionsDropdown.new(base_url:, product:)
+          'insomnia_link' => insomnia_link
         }.merge(@frontmatter)
       end
 
       private
+
+      def insomnia_link
+        @insomnia_link ||= Drops::OAS::InsomniaLink.new(
+          label: api_spec.title,
+          version:,
+          site:,
+          page_relative_path: relative_path.dup
+        )
+      end
 
       def namespace
         @namespace ||= @file.split('/')[1]
---
+++ app/_plugins/generators/data/api_specs.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Data
+    class APISpecs
+      attr_reader :site, :page
+
+      def initialize(site:, page:)
+        @site = site
+        @page = page
+      end
+
+      def process
+        return nil unless @page.data['api_specs']
+
+        @page.data['api_specs'] = api_specs
+      end
+
+      private
+
+      def api_specs
+        @page.data['api_specs'].map do |spec|
+          api_spec = api_page(spec)
+          raise ArgumentError, "There's no API in app/_api/ that matches #{spec}" unless api_spec
+
+          Drops::APISpecLink.new(api_spec:, site:)
+        end
+      end
+
+      def api_page(spec)
+        site
+          .data
+          .fetch('ssg_api_pages', [])
+          .detect { |page| page.url == "/api/#{spec}/" }
+      end
+    end
+  end
+end
