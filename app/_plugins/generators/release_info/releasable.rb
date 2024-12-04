538aed5654ae12da5eb26e938abdcaa19f50c6e2
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-12-04 14:57:07 +0100

    Refactor release's info for products and tools

---
+++ app/_plugins/generators/release_info/releasable.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module ReleaseInfo
+    module Releasable
+      def releases
+        @releases ||= available_releases.select do |r|
+          Utils::Version.in_range?(r['release'], min: min_release, max: max_release)
+        end
+      end
+
+      def latest_available_release
+        @latest_available_release ||= available_releases.detect(&:latest?)
+      end
+
+      def min_release
+        @min_release ||= min_version && available_releases.detect { |r| r['release'] == min_version.to_s }
+      end
+
+      def max_release
+        @max_release ||= max_version && available_releases.detect { |r| r['release'] == max_version.to_s }
+      end
+
+      def latest_release_in_range
+        @latest_release_in_range ||= begin
+          return min_release if min_release && min_release > latest_available_release
+          return max_release if max_release && max_release < latest_available_release
+
+          latest_available_release
+        end
+      end
+
+      private
+
+      def min_version
+        @min_version[key]
+      end
+
+      def max_version
+        @max_version[key]
+      end
+    end
+  end
+end
