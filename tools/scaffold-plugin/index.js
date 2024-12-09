be1927878a5476eae232580ceede40a2b6e3b4e7
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-12-09 13:46:10 +0100

    [Hub] Add tool (and make command) that scaffolds a plugin
    Usage:
    
    make scaffold-plugin PLUGIN=my-new-plugin

---
+++ tools/scaffold-plugin/index.js
@@ -0,0 +1,54 @@
+import minimist from "minimist";
+import { promises as fs } from "fs";
+import path from "path";
+
+const argv = minimist(process.argv.slice(2));
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+(async function () {
+  const pluginName = argv._[0];
+  const toolDir = path.resolve(__dirname);
+  const rootDir = path.resolve(toolDir, "../../");
+  const templatesFolder = path.resolve(toolDir, "./templates");
+
+  if (!pluginName) {
+    console.error("Please provide the name of the plugin.");
+    process.exit(1);
+  }
+
+  const pluginFolder = path.resolve(rootDir, `app/_kong_plugins/${pluginName}`);
+
+  try {
+    console.log(`Scaffolding plugin: ${pluginName}...`);
+    await fs.mkdir(pluginFolder);
+    console.log(`Plugin folder created at: ${pluginFolder}`);
+
+    const items = await fs.readdir(templatesFolder, { withFileTypes: true });
+
+    console.log("Copying templates...");
+    for (const item of items) {
+      if (item.isDirectory()) {
+        await fs.mkdir(path.join(pluginFolder, item.name));
+        const subFolder = path.join(templatesFolder, item.name);
+        const files = await fs.readdir(subFolder);
+
+        for (const file of files) {
+          await fs.copyFile(
+            path.join(subFolder, file),
+            path.join(pluginFolder, item.name, file)
+          );
+        }
+      } else {
+        await fs.copyFile(
+          path.join(templatesFolder, item.name),
+          path.join(pluginFolder, item.name)
+        );
+      }
+      console.log(`  * ${item.name}`);
+    }
+    console.log("Done!");
+  } catch (error) {
+    console.error("Error creating the plugin folder or files:", error);
+    process.exit(1);
+  }
+})();
