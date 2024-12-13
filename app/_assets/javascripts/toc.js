7adaa991ce9560ec4ce79c484c51803eb23bdeec
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-12-13 14:44:09 +0100

    Refactor tab styles

--- app/_assets/javascripts/toc.js
+++ app/_assets/javascripts/toc.js
@@ -1,49 +1,46 @@
 function toggleTocLinkClasses(link, isActive) {
-  const activeClasses = ['border-l-2', 'border-brand'];
-  const activeLinkClasses = ['text-primary', '-ml-0.5'];
+  const activeClasses = ["tab-button__vertical--active"];
 
   if (isActive) {
-      link.classList.add(...activeLinkClasses);
-      link.classList.remove('text-secondary');
-      link.parentElement.classList.add(...activeClasses);
+    link.classList.add(...activeClasses);
   } else {
-      link.classList.add('text-secondary');
-      link.classList.remove(...activeLinkClasses);
-      link.parentElement.classList.remove(...activeClasses);
+    link.classList.remove(...activeClasses);
   }
 }
 
 window.addEventListener("scroll", () => {
-    const anchors = document.querySelectorAll("a.header-link");
-    const scrollToLinks = document.querySelectorAll("a.scroll-to");
-    const navHeight = document.getElementById('header-nav').offsetHeight;
+  const anchors = document.querySelectorAll("a.header-link");
+  const scrollToLinks = document.querySelectorAll("a.scroll-to");
+  const navHeight = document.getElementById("header-nav").offsetHeight;
 
-    if (!anchors.length || !scrollToLinks.length) {
-      return;
-    }
+  if (!anchors.length || !scrollToLinks.length) {
+    return;
+  }
 
-    let activeSet = false;
+  let activeSet = false;
 
-    scrollToLinks.forEach(link => toggleTocLinkClasses(link, false));
+  scrollToLinks.forEach((link) => toggleTocLinkClasses(link, false));
 
-    // Convert NodeList to Array and reverse it
-    const anchorsArray = Array.from(anchors).reverse();
+  // Convert NodeList to Array and reverse it
+  const anchorsArray = Array.from(anchors).reverse();
 
-    for (const element of anchorsArray) {
-      const elementTop = element.getBoundingClientRect().top + window.scrollY;
+  for (const element of anchorsArray) {
+    const elementTop = element.getBoundingClientRect().top + window.scrollY;
 
-      // window top + header section + extra padding
-      if (window.scrollY + navHeight + 20 >= elementTop) {
-        const matchingLink = document.querySelector(`a.scroll-to[href$="${element.getAttribute("href")}"]`);
-        if (matchingLink) {
-          toggleTocLinkClasses(matchingLink, true);
-          activeSet = true;
-        }
-        break;
+    // window top + header section + extra padding
+    if (window.scrollY + navHeight + 20 >= elementTop) {
+      const matchingLink = document.querySelector(
+        `a.scroll-to[href$="${element.getAttribute("href")}"]`
+      );
+      if (matchingLink) {
+        toggleTocLinkClasses(matchingLink, true);
+        activeSet = true;
       }
-    };
-
-    if (!activeSet) {
-      toggleTocLinkClasses(scrollToLinks[0], true);
+      break;
     }
-  });
+  }
+
+  if (!activeSet) {
+    toggleTocLinkClasses(scrollToLinks[0], true);
+  }
+});
