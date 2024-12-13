a75e6af4ef9d929e838580c09c4b01a924e71693
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2024-12-13 09:24:28 +0100

    [Hub] `Get Started` links to first example

--- app/_plugins/generators/plugins/generator.rb
+++ app/_plugins/generators/plugins/generator.rb
@@ -63,12 +63,6 @@ module Jekyll
 
           site.pages << example
         end
-
-        examples = Jekyll::PluginPages::Pages::Examples
-                   .new(plugin:, file: '')
-                   .to_jekyll_page
-
-        site.pages << examples
       end
     end
   end
