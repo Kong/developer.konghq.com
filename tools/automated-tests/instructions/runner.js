9e411010a30a8ec30b7891b9cf32e0604c2f06c9
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-01-13 13:15:17 +0100

    Add debug statements

--- tools/automated-tests/instructions/runner.js
+++ tools/automated-tests/instructions/runner.js
@@ -8,7 +8,10 @@ import { fileURLToPath } from "url";
 import { processPrereqs } from "./prereqs.js";
 import { processCleanup } from "./cleanup.js";
 import { processSteps } from "./step.js";
+import debug from "debug";
 
+const log = debug("runner");
+const debugLog = debug("debug");
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 function executeCommand(container, cmd) {
@@ -22,11 +25,18 @@ function executeCommand(container, cmd) {
 
       await new Promise((resolve, reject) => {
         execCommand.start({}, (err, stream) => {
+          let output = "";
           if (err) {
             reject(err);
           }
-          stream.pipe(process.stdout);
-          stream.on("end", resolve);
+          //stream.pipe(debugLog);
+          stream.on("data", (chunk) => {
+            output += chunk.toString();
+          });
+          stream.on("end", function () {
+            debugLog(output);
+            resolve();
+          });
         });
       });
 
@@ -35,7 +45,7 @@ function executeCommand(container, cmd) {
       if (execInfo.ExitCode === 0) {
         resolve();
       } else {
-        console.error("Command failed with exit code:", execInfo.ExitCode);
+        log("Command failed with exit code:", execInfo.ExitCode);
         reject(`Failed to run command ${cmd}`);
       }
     } catch (error) {
@@ -47,27 +57,27 @@ function executeCommand(container, cmd) {
 async function stopContainer(container) {
   if (container) {
     await container.stop();
-    console.log("Container stopped.");
+    log("Container stopped.");
   }
 }
 
 async function removeContainer(container) {
   if (container) {
     await container.remove();
-    console.log("Container removed.");
+    log("Container removed.");
   }
 }
 
 async function fetchImage(docker, setupConfig) {
   const imageName = setupConfig.image;
-  console.log(`Fetching the image ${imageName}...`);
+  log(`Fetching the image ${imageName}...`);
   try {
     const image = await docker.getImage(imageName);
     await image.inspect();
-    console.log(`Image '${imageName}' already exists.`);
+    log(`Image '${imageName}' already exists.`);
     return;
   } catch (err) {
-    console.log(`Image '${imageName}' not found, building it...`);
+    log(`Image '${imageName}' not found, building it...`);
 
     return new Promise((resolve, reject) => {
       const dockerContext = join(
@@ -116,12 +126,12 @@ async function runConfig(config, container) {
 }
 
 async function runSetup(config, container) {
-  console.log("Setting things up...");
+  log("Setting things up...");
   await runConfig(config, container);
 }
 
 async function runPrereqs(prereqs, container) {
-  console.log("Running prereqs...");
+  log("Running prereqs...");
   if (prereqs) {
     const config = await processPrereqs(prereqs);
     await runConfig(config, container);
@@ -129,7 +139,7 @@ async function runPrereqs(prereqs, container) {
 }
 
 async function runCleanup(cleanup, container) {
-  console.log("Cleaning up...");
+  log("Cleaning up...");
   if (cleanup) {
     const config = await processCleanup(cleanup);
     await runConfig(config, container);
@@ -137,7 +147,7 @@ async function runCleanup(cleanup, container) {
 }
 
 async function runSteps(steps, container) {
-  console.log("Running steps...");
+  log("Running steps...");
   if (steps) {
     const config = await processSteps(steps);
     await runConfig(config, container);
@@ -166,10 +176,10 @@ export async function runInstructions(instructions) {
         NetworkMode: "host",
       },
     });
-    console.log("Container created with ID:", container.id);
+    log("Container created with ID:", container.id);
 
     await container.start();
-    console.log("Container started.");
+    log("Container started.");
 
     await runSetup(setupConfig, container);
 
