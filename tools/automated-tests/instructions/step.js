79993a544828af9aec87c0d91fabf217b4f56827
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-01-14 12:45:43 +0100

    [Automated Tests] Add support for running rate-limit-check and some fixes

--- tools/automated-tests/instructions/step.js
+++ tools/automated-tests/instructions/step.js
@@ -1,12 +1,3 @@
 export async function processSteps(steps) {
-  let commands = [];
-  for (const step of steps) {
-    if (typeof step === "object") {
-      // TODO: process step of type object
-      console.warn("Unsupported: Step of type object");
-    } else {
-      commands.push(step);
-    }
-  }
-  return { commands };
+  return { commands: steps };
 }
