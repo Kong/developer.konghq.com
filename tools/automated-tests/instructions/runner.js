f40266425a56016f3bb43cd71aec6fdf1796afcd
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-01-15 09:15:47 +0100

    [Automated Tests] Refactor how tests are run
    * Generate instructions files first
    * For each runtime, run all the tests, e.g. gateway, konnect, etc
      * setup the corresponding docker image
      * run the corresponding tests sequentially
      * before each test, reset the runtime
      * after all the tests, clean up the runtime
    
    Config.yaml
    
    * Added the current gateway version to test against, we'll be able to
      override this one with env variables
    * Added a list of how-tos to test for now, until we can tag them
      automatically

--- tools/automated-tests/instructions/runner.js
+++ tools/automated-tests/instructions/runner.js
@@ -1,117 +1,12 @@
-import Dockerode from "dockerode";
-import { join, dirname } from "path";
-import { getSetupConfig } from "./setup.js";
-import fg from "fast-glob";
 import fs from "fs/promises";
 import yaml from "js-yaml";
-import { fileURLToPath } from "url";
-import { processPrereqs } from "./prereqs.js";
-import { processCleanup } from "./cleanup.js";
-import { processSteps } from "./step.js";
 import debug from "debug";
+import { processPrereqs } from "./prereqs.js";
+import { processSteps } from "./step.js";
 import { validate, ValidationError } from "./validations.js";
+import { executeCommand } from "../docker-helper.js";
 
-const log = debug("runner");
-const debugLog = debug("debug");
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-async function executeCommand(container, cmd) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const execCommand = await container.exec({
-        Cmd: ["bash", "-c", cmd],
-        AttachStdout: true,
-        AttachStderr: true,
-      });
-
-      await new Promise((resolve, reject) => {
-        execCommand.start({}, (err, stream) => {
-          let output = "";
-          if (err) {
-            return reject(err);
-          }
-          stream.on("data", (chunk) => {
-            output += chunk.toString();
-          });
-          stream.on("end", function () {
-            debugLog(output);
-            return resolve(output);
-          });
-        });
-      });
-
-      const execInfo = await execCommand.inspect();
-
-      if (execInfo.ExitCode === 0) {
-        resolve();
-      } else {
-        log("Command failed with exit code:", execInfo.ExitCode);
-        reject(`Failed to run command ${cmd}`);
-      }
-    } catch (error) {
-      throw error;
-    }
-  });
-}
-
-async function stopContainer(container) {
-  if (container) {
-    await container.stop();
-    log("Container stopped.");
-  }
-}
-
-async function removeContainer(container) {
-  if (container) {
-    await container.remove();
-    log("Container removed.");
-  }
-}
-
-async function fetchImage(docker, setupConfig) {
-  const imageName = setupConfig.image;
-  log(`Fetching the image ${imageName}...`);
-  try {
-    const image = await docker.getImage(imageName);
-    await image.inspect();
-    log(`Image '${imageName}' already exists.`);
-    return;
-  } catch (err) {
-    log(`Image '${imageName}' not found, building it...`);
-
-    return new Promise((resolve, reject) => {
-      const dockerContext = join(
-        __dirname,
-        "../",
-        "docker",
-        setupConfig.product
-      );
-
-      docker.buildImage(
-        {
-          context: dockerContext,
-          src: ["Dockerfile"],
-        },
-        { t: imageName },
-        function (error, stream) {
-          if (error) {
-            return reject(error);
-          }
-
-          docker.modem.followProgress(stream, onFinished, onProgress);
-
-          function onFinished(err, output) {
-            if (err) {
-              return reject(err);
-            }
-            return resolve(output);
-          }
-          function onProgress(event) {}
-        }
-      );
-    });
-  }
-}
+const log = debug("tests:runner");
 
 async function runConfig(config, container) {
   try {
@@ -125,25 +20,12 @@ async function runConfig(config, container) {
   }
 }
 
-async function runSetup(config, container) {
-  log("Setting things up...");
-  await runConfig(config, container);
-}
-
 async function runPrereqs(prereqs, container) {
   log("Running prereqs...");
   if (prereqs) {
     const config = await processPrereqs(prereqs);
     await runConfig(config, container);
     log(`   prereq ✅ .`);
-  }
-}
-
-async function runCleanup(cleanup, container) {
-  log("Cleaning up...");
-  if (cleanup) {
-    const config = await processCleanup(cleanup);
-    await runConfig(config, container);
   }
 }
 
@@ -171,64 +53,26 @@ async function runSteps(steps, container) {
   }
 }
 
-export async function runInstructions(instructions) {
-  let container;
-
+export async function runInstructions(instructions, container) {
   try {
-    const setupConfig = await getSetupConfig(instructions);
-    const image = setupConfig.image;
-
-    const docker = new Dockerode({
-      socketPath: "/var/run/docker.sock",
-    });
-
-    await fetchImage(docker, setupConfig);
-
-    container = await docker.createContainer({
-      Image: image,
-      Tty: true,
-      ENV: setupConfig.env,
-      HostConfig: {
-        Binds: ["/var/run/docker.sock:/var/run/docker.sock"],
-        NetworkMode: "host",
-      },
-    });
-    log("Container created with ID:", container.id);
-
-    await container.start();
-    log("Container started.");
-
-    await runSetup(setupConfig, container);
-
     await runPrereqs(instructions.prereqs, container);
 
     await runSteps(instructions.steps, container);
-
-    await runCleanup(setupConfig, container);
-
-    await stopContainer(container);
-
-    await removeContainer(container);
   } catch (err) {
-    await stopContainer(container);
-    await removeContainer(container);
     if (err instanceof ValidationError) {
       console.error(err.message);
     } else {
       console.error("Error: ", err);
     }
-    process.exit(1);
+    if (!process.env.CONTINUE_ON_ERROR) {
+      process.exit(1);
+    }
   }
 }
 
-export async function runInstructionsFile(file) {
+export async function runInstructionsFile(file, container) {
+  log(`Running file: ${file}`);
   const fileContent = await fs.readFile(file, "utf8");
   const instructions = yaml.load(fileContent);
-  await runInstructions(instructions);
+  await runInstructions(instructions, container);
 }
-
-(async function main() {
-  await runInstructionsFile(
-    "output/instructions/how-to/add-rate-limiting-for-a-consumer-with-kong-gateway/on-prem.yaml"
-  );
-})();
