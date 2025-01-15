556a374e5795e9d09acae7ba94089867eabddd87
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-01-15 11:11:43 +0100

    [Automated Tests] Check if the version in the instructions file is greater thatn or equal to the one we are running, if not, skip the test.

--- tools/automated-tests/instructions/runner.js
+++ tools/automated-tests/instructions/runner.js
@@ -5,8 +5,27 @@ import { processPrereqs } from "./prereqs.js";
 import { processSteps } from "./step.js";
 import { validate, ValidationError } from "./validations.js";
 import { executeCommand } from "../docker-helper.js";
+import { getSetupConfig } from "./setup.js";
 
 const log = debug("tests:runner");
+
+function compareVersions(v1, v2) {
+  const v1Parts = v1.split(".");
+  const v2Parts = v2.split(".");
+
+  for (let i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
+    const part1 = parseInt(v1Parts[i] || 0);
+    const part2 = parseInt(v2Parts[i] || 0);
+
+    if (part1 > part2) {
+      return 1; // v1 is greater
+    } else if (part1 < part2) {
+      return -1; // v2 is greater
+    }
+  }
+
+  return 0; // Versions are equal
+}
 
 async function runConfig(config, container) {
   try {
@@ -18,6 +37,29 @@ async function runConfig(config, container) {
   } catch (error) {
     throw error;
   }
+}
+
+async function checkSetup(setup, runtimeConfig) {
+  const { runtime, version: minVersion } = await getSetupConfig(setup);
+  if (runtime !== runtimeConfig.runtime) {
+    throw new Error(
+      "The instructions files setup does not match the current runtime."
+    );
+  }
+
+  if (minVersion && runtimeConfig.version) {
+    // The version specified in the instructions file is the min_version supported.
+    // Given that we generate the file once and run it against different versions,
+    // we need to skip the case when the min_version is greater than the current version
+    // that we are running.
+    if (compareVersions(minVersion, runtimeConfig.version) === 1) {
+      log(
+        `⚠️ Skipping test. The version in 'setup' is greater than ${runtimeConfig.version}`
+      );
+      return false;
+    }
+  }
+  return true;
 }
 
 async function runPrereqs(prereqs, container) {
@@ -53,8 +95,14 @@ async function runSteps(steps, container) {
   }
 }
 
-export async function runInstructions(instructions, container) {
+export async function runInstructions(instructions, runtimeConfig, container) {
   try {
+    const check = await checkSetup(instructions.setup, runtimeConfig);
+
+    if (!check) {
+      return;
+    }
+
     await runPrereqs(instructions.prereqs, container);
 
     await runSteps(instructions.steps, container);
@@ -70,9 +118,9 @@ export async function runInstructions(instructions, container) {
   }
 }
 
-export async function runInstructionsFile(file, container) {
+export async function runInstructionsFile(file, runtimeConfig, container) {
   log(`Running file: ${file}`);
   const fileContent = await fs.readFile(file, "utf8");
   const instructions = yaml.load(fileContent);
-  await runInstructions(instructions, container);
+  await runInstructions(instructions, runtimeConfig, container);
 }
