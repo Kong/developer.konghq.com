7e1df9a07c3f4f168f8c113bced0eb204fd89393
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-01-21 18:58:27 +0100

    Add support for `directives` to kong_config_table and raise an error if they don't have a description

--- app/_plugins/drops/kong_config_table.rb
+++ app/_plugins/drops/kong_config_table.rb
@@ -18,7 +18,7 @@ module Jekyll
         end
 
         def default_value
-          @default_value ||= @field.fetch('defaultValue')
+          @default_value ||= @field['defaultValue']
         end
 
         def array?
@@ -35,14 +35,35 @@ module Jekyll
       def initialize(config, release_number) # rubocop:disable Lint/MissingSuper
         @config = config
         @release_number = release_number
+
+        validate_config!
       end
 
       def fields
-        @fields ||= @config.map { |c| KongConfigField.new(c, kong_conf[c['name']]) }
+        @fields ||= (params + directives).sort_by(&:name)
+      end
+
+      def params
+        @params ||= @config.fetch('config', []).map { |c| KongConfigField.new(c, kong_conf[c['name']]) }
+      end
+
+      def directives
+        @directives ||= @config.fetch('directives', []).map { |c| KongConfigField.new(c, kong_conf[c['name']]) }
       end
 
       def kong_conf
         @kong_conf ||= site.data.dig('kong-conf', @release_number.gsub('.', ''))
+      end
+
+      private
+
+      def validate_config!
+        @config.fetch('directives', []).each do |d|
+          unless d.key?('description')
+            raise ArgumentError,
+                  "Missing description for directive `#{d}` in kong_config_table"
+          end
+        end
       end
     end
   end
