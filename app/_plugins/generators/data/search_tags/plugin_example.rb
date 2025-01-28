d1bde671be00f6efa1b6c9cded53d1f1d50df581
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-01-28 07:59:52 +0100

    Set plugin's example metadata for algolia

--- app/_plugins/generators/data/search_tags/plugin_example.rb
+++ app/_plugins/generators/data/search_tags/plugin_example.rb
@@ -5,7 +5,10 @@ require_relative 'base'
 module Jekyll
   module Data
     module SearchTags
-      class PluginExample < Base
+      class PluginExample < Base # rubocop:disable Style/Documentation
+        def search_data
+          super.merge('title' => @page.data['example_title'])
+        end
       end
     end
   end
