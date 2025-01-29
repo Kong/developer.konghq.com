78901382f2f56b44178e9f8f12f6d49872ce3258
lena-larionova <54370747+lena-larionova@users.noreply.github.com>
2025-01-29 09:22:57 -0800

    Security update process doc (#319)
    * security update process doc
    
    * add entry in sources.yaml
    
    * vale
    
    * rename page path and adjust metadata
    
    * Add new content_type: policy
    
    * Fix issue with edit and issue links when there aren't any breadcrumbs
    
    * Fix policy pages spacing and remove edit link from the bottom of the
    page
    
    ---------
    
    Co-authored-by: Angel <Guaris@users.noreply.github.com>
    Co-authored-by: Fabian Rodriguez <fabian.rodriguez@konghq.com>

--- app/_plugins/generators/data/edit_link/base.rb
+++ app/_plugins/generators/data/edit_link/base.rb
@@ -3,7 +3,7 @@
 module Jekyll
   module Data
     module EditLink
-      class Base
+      class Base # rubocop:disable Style/Documentation
         def initialize(site:, page:)
           @site = site
           @page = page
@@ -16,7 +16,9 @@ module Jekyll
         private
 
         def edit_link
-          @edit_link ||= "#{repo_edit_url}/#{@page.relative_path}"
+          return if @page.data['content_type'] == 'policy'
+
+          "#{repo_edit_url}/#{@page.relative_path}"
         end
 
         def repo_edit_url
---
+++ app/_plugins/generators/data/search_tags/policy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module Jekyll
+  module Data
+    module SearchTags
+      class Policy < Base
+      end
+    end
+  end
+end
