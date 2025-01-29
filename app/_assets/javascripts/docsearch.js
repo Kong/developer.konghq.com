c23db97b594c9d9311ff16dc2ad1c8ae6ec1e94e
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-01-29 16:08:25 +0100

    Add some tweaks to docsearch
    Overwrite the description, disable customization and increase the number of
    results.
    Also fixes an issue with one of Algolia's classes.
    
    TODO: this will not be necesary when we add a custom extraction.

--- app/_assets/javascripts/docsearch.js
+++ app/_assets/javascripts/docsearch.js
@@ -7,11 +7,18 @@ docsearch({
   appId: "Z2JDSBZWKU",
   indexName: "kongdeveloper",
   apiKey: "7eaf59d4529f8b3bb44e5a8556aac227",
+  maxResultsPerGroup: 10,
+  disableUserPersonalization: true,
   transformItems(items) {
     return items.map((item) => {
       const urlObj = new URL(item.url);
       const path = `${urlObj.pathname}${urlObj.hash}`;
-      return { ...item, url: path };
+
+      const content = item.content || item.description;
+      return { ...item, url: path, content };
     });
+  },
+  searchParameters: {
+    attributesToRetrieve: ["*"],
   },
 });
