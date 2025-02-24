b910c4b54f7a46cceb863f4ce0ab1d92bfabf33e
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-02-23 23:38:00 -0300

    Add {% konnect_roles_table %} Renders a table with the roles + description of the provided schema
    Usage:
    Pass the name of the schema as defined in
    `api-specs/konnect/identity/vX/openapi.yaml`
    
    The name of the schemas can be found under:
    `components.responses.Roles.content.application/json.schema.properties`
    
    ```
    {% konnect_roles_table %}
    schema: audit_logs
    {% endkonnect_roles_table %}
    ```

---
+++ app/_plugins/blocks/konnect_roles_table.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+module Jekyll
+  class KonnectRolesTable < Liquid::Block # rubocop:disable Style/Documentation
+    def initialize(tag_name, markup, tokens)
+      super
+      @name = markup.strip
+    end
+
+    def render(context) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      @context = context
+      @site = context.registers[:site]
+      @page = context.environments.first['page']
+
+      contents = super
+      config = YAML.load(contents)
+      drop = Drops::KonnectRolesTable.new(config)
+
+      context.stack do
+        context['config'] = drop
+        Liquid::Template.parse(template).render(context)
+      end
+    rescue Psych::SyntaxError => e
+      message = <<~STRING
+        On `#{@page['path']}`, the following {% konnect_roles_table %} block contains a malformed yaml:
+        #{contents.strip.split("\n").each_with_index.map { |l, i| "#{i}: #{l}" }.join("\n")}
+        #{e.message}
+      STRING
+      raise ArgumentError, message
+    end
+
+    def template
+      @template ||= File.read(File.expand_path('app/_includes/components/konnect_roles_table.html'))
+    end
+  end
+end
+
+Liquid::Template.register_tag('konnect_roles_table', Jekyll::KonnectRolesTable)
--- app/konnect/teams-and-roles.md
+++ app/konnect/teams-and-roles.md
@@ -20,3 +20,34 @@ Pull content from:
 * https://docs.konghq.com/konnect/org-management/teams-and-roles/manage/
 * https://docs.konghq.com/konnect/org-management/teams-and-roles/teams-reference/
 * https://docs.konghq.com/konnect/org-management/teams-and-roles/roles-reference/
+
+
+## API Products
+
+{% konnect_roles_table %}
+schema: api_products
+{% endkonnect_roles_table %}
+
+## Control Planes
+
+{% konnect_roles_table %}
+schema: control_planes
+{% endkonnect_roles_table %}
+
+## Audit Logs
+
+{% konnect_roles_table %}
+schema: audit_logs
+{% endkonnect_roles_table %}
+
+## Identity
+
+{% konnect_roles_table %}
+schema: identity
+{% endkonnect_roles_table %}
+
+## Mesh control planes
+
+{% konnect_roles_table %}
+schema: mesh_control_planes
+{% endkonnect_roles_table %}
