f46a2efb2db8f7361b10be8841123ed0dfd5a149
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-02-28 11:46:53 -0300

    Replace docsearch with autocomplete and use a custom renderer

---
+++ app/_assets/javascripts/apps/components/SearchModalResultItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="flex w-full">
+    <a :href="relativeUrl" class="flex w-full p-3 rounded-md bg-primary border border-primary/5 shadow-primary hover:no-underline hover:border-brand-saturated/40 hover:shadow-hover-card  focus:border-brand-saturated/40 focus:shadow-hover-card">
+      <div class="flex flex-col w-full gap-2">
+        <div class="text-terciary text-xs">
+          {{ breadcrumbs }}
+        </div>
+        <div class="text-primary font-bold text-base">
+          {{ heading }}
+        </div>
+        <div class="text-secondary text-sm">
+          <SearchResultSnippet :item="item" attribute="content" />
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+import SearchResultSnippet from './SearchResultSnippet.vue';
+
+export default {
+  props: {
+    item: Object,
+  },
+  components: {
+    SearchResultSnippet
+  },
+  computed: {
+    heading() {
+      if (this.item.content_type === 'plugin_example') {
+        return this.item.title;
+      }
+      const levels = Object.entries(this.item.hierarchy)
+        .filter(([key, value]) => key !== 'lvl0' && value !== null);
+      if (levels.length > 0) {
+        return levels[levels.length - 1][1];
+      }
+      return ''; // Or handle the case where there are no levels
+    },
+    breadcrumbs() {
+      const levels = Object.entries(this.item.hierarchy)
+        .filter(([key, value]) => key !== 'lvl0' && value !== null)
+        .map(([key, value]) => value);
+
+      if (this.item.content_type === 'plugin') {
+        levels.unshift('Plugins')
+      }
+      if (this.item.content_type === 'how_to') {
+        levels.unshift('How-to Guides')
+      }
+      return levels.join(' > ');
+    },
+    relativeUrl() {
+      const urlObj = new URL(this.item.url);
+      return `${urlObj.pathname}${urlObj.hash}`;
+    },
+  },
+};
+</script>
---
+++ app/_assets/javascripts/apps/components/SearchResultSnippet.vue
@@ -0,0 +1,22 @@
+<script>
+import { h } from 'vue';
+import { parseAlgoliaHitSnippet } from '@algolia/autocomplete-preset-algolia';
+
+export default {
+  props: {
+    item: Object,
+    attribute: String
+  },
+  render() {
+    return h(
+      'span',
+      {},
+      parseAlgoliaHitSnippet({ hit: this.item, attribute: this.attribute }).map((x, index) =>
+        x.isHighlighted
+          ? h('strong', { key: index }, x.value)
+          : x.value
+      )
+    );
+  }
+};
+</script>
