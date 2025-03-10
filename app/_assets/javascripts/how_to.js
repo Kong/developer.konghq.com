872050a6607a14057da646b20e055b4bcf7520ee
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-03-10 09:14:00 +0100

    Check if cleanup is present before toggling them

--- app/_assets/javascripts/how_to.js
+++ app/_assets/javascripts/how_to.js
@@ -1,60 +1,75 @@
 class HowTo {
-    constructor() {
-        this.deploymentTopologySwitch = document.getElementById('deployment-topology-switch');
-        this.prerequisites = document.querySelector('.prerequisites');
-        this.cleanup = document.querySelector('.cleanup');
+  constructor() {
+    this.deploymentTopologySwitch = document.getElementById(
+      "deployment-topology-switch"
+    );
+    this.prerequisites = document.querySelector(".prerequisites");
+    this.cleanup = document.querySelector(".cleanup");
 
-        this.init();
-        this.addEventListeners();
+    this.init();
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    if (this.deploymentTopologySwitch) {
+      this.deploymentTopologySwitch.addEventListener(
+        "change",
+        this.onChange.bind(this)
+      );
     }
+  }
 
-    addEventListeners() {
-        if (this.deploymentTopologySwitch) {
-            this.deploymentTopologySwitch.addEventListener('change', this.onChange.bind(this));
-        }
+  init() {
+    if (this.deploymentTopologySwitch) {
+      this.toggleTopology(this.deploymentTopologySwitch.value, false);
     }
+  }
 
-    init() {
-        if (this.deploymentTopologySwitch) {
-            this.toggleTopology(this.deploymentTopologySwitch.value, false);
-        }
-    }
+  onChange(event) {
+    this.toggleTopology(event.target.value, true);
+  }
 
-    onChange(event) {
-        this.toggleTopology(event.target.value, true);
-    }
+  toggleTopology(topology, trigger) {
+    this.prerequisites
+      .querySelectorAll("[data-deployment-topology]")
+      .forEach((item) => {
+        this.toggleItem(item, topology, trigger);
+      });
 
-    toggleTopology(topology, trigger) {
-        this.prerequisites.querySelectorAll('[data-deployment-topology]').forEach((item) => {
-            this.toggleItem(item, topology, trigger);
-        })
+    document
+      .querySelectorAll(
+        ":not(.prerequisites,.cleanup) [data-deployment-topology]"
+      )
+      .forEach((item) => {
+        this.toggleItem(item, topology);
+      });
 
-        document.querySelectorAll(':not(.prerequisites,.cleanup) [data-deployment-topology]').forEach((item) => {
-            this.toggleItem(item, topology);
+    if (this.cleanup) {
+      this.cleanup
+        .querySelectorAll("[data-deployment-topology]")
+        .forEach((item) => {
+          this.toggleItem(item, topology, trigger);
         });
-
-        this.cleanup.querySelectorAll('[data-deployment-topology]').forEach((item) => {
-            this.toggleItem(item, topology, trigger);
-        })
     }
+  }
 
-    toggleItem(item, topology, trigger) {
-        if (item.dataset.deploymentTopology === topology) {
-            item.classList.remove('hidden');
-            if (trigger) {
-                const accordionTrigger = item.querySelector('.accordion-trigger');
-                if (accordionTrigger) {
-                    accordionTrigger.click();
-                }
-            }
-        } else {
-            item.classList.add('hidden');
+  toggleItem(item, topology, trigger) {
+    if (item.dataset.deploymentTopology === topology) {
+      item.classList.remove("hidden");
+      if (trigger) {
+        const accordionTrigger = item.querySelector(".accordion-trigger");
+        if (accordionTrigger) {
+          accordionTrigger.click();
         }
+      }
+    } else {
+      item.classList.add("hidden");
     }
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    if (document.querySelector('.how-to')) {
-        new HowTo();
-    }
-})
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector(".how-to")) {
+    new HowTo();
+  }
+});
