d7810afe31c938b9931987876e590dc315e0e0db
Vaibhav <vaibhav.rajsingh@konghq.com>
2025-03-12 22:12:28 +0530

    feat: use kong spec-renderer v1.88.0 package

--- app/_assets/entrypoints/api_spec.js
+++ app/_assets/entrypoints/api_spec.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import APISpec from "~/javascripts/apps/APISpec.vue";
-import "@kong/spec-renderer-dev/dist/style.css";
+import "@kong/spec-renderer/dist/style.css";
 
 if (document.getElementById("api-spec") !== null) {
   const app = createApp(APISpec);
