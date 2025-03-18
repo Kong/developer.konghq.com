df003bfce588b8a0e3baf0fe30be0e50ce50b71a
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-03-18 17:58:22 +0100

    Fix releases dropdown

--- app/_assets/javascripts/dropdowns.js
+++ app/_assets/javascripts/dropdowns.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const releasesDropdown = document.getElementById("releases-dropdown");
-  if (releasesDropdown) {
-    releasesDropdown.addEventListener("change", function () {
-      const url = releasesDropdown.value;
-      if (url) {
-        window.location.href = url;
+  document
+    .querySelectorAll(".releases-dropdown")
+    .forEach(function (releasesDropdown) {
+      if (releasesDropdown) {
+        releasesDropdown.addEventListener("change", function () {
+          const url = releasesDropdown.value;
+          if (url) {
+            window.location.href = url;
+          }
+        });
       }
     });
-  }
 
   const pluginExampleDropdown = document.getElementById(
     "plugin-example-dropdown"
