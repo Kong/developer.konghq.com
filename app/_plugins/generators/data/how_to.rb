2c93456d8c835e3de5a1d38447bb50020ef3ced2
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-03-21 11:29:35 +0100

    Skip how-tos that have a min_version that hasn't been released from being published in production.
    They do get generated in dev and preview apps under `/url/unreleased/`
    where unreleased is the label that the corresponding `min_version` has.

--- app/_plugins/generators/data/how_to.rb
+++ app/_plugins/generators/data/how_to.rb
@@ -12,15 +12,28 @@ module Jekyll
 
       def process
         @page.data['latest_release'] = release_info.latest_available_release
+        return unless unreleased?
+
+        @page.instance_variable_set(:@url, "#{@page.url}/#{release_info.min_release}/")
+
+        return unless ENV['JEKYLL_ENV'] == 'production'
+
+        @page.data['published'] = false
       end
 
       def release_info
         @release_info ||= ReleaseInfo::Product.new(
           site:,
           product: @page.data['products'].first,
-          min_version: @page.data['min_version'],
-          max_version: @page.data['max_version']
+          min_version: @page.data.fetch('min_version', {}),
+          max_version: @page.data.fetch('max_version', {})
         )
+      end
+
+      def unreleased?
+        return false unless @page.data['min_version']
+
+        @release_info.unreleased?
       end
     end
   end
