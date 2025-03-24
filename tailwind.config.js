44ee27944336ba77cec296b351a02d65b8e57d7f
Michael Heap <m@michaelheap.com>
2025-03-24 09:52:30 +0000

    Support recursive grids in landing pages (#878)
    * Rename col_span to column_count
    
    * Add col_span to Landing Page columns
    
    * Add landing page grid component

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,14 @@ module.exports = {
       pattern: /grid-cols-[1-6]/,
       variants: ["md", "lg"],
     },
+    {
+      pattern: /col-span-[1-6]/,
+      variants: ["md", "lg"],
+    },
+    {
+      pattern: /gap-[1-12]/,
+      variants: ["md", "lg"],
+    },
     "self-start",
     "self-center",
     "self-end",
