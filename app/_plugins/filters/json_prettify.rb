d03c87c3668645b721fd36c26705c27555fda131
Michael Heap <m@michaelheap.com>
2025-03-25 13:44:34 +0000

    JSON formatting was one space off

--- app/_plugins/filters/json_prettify.rb
+++ app/_plugins/filters/json_prettify.rb
@@ -4,13 +4,13 @@ require 'json'
 
 module Jekyll
   module JSONPrettifyFilter
-    def json_prettify(input, last_line_spacing = nil)
+    def json_prettify(input, indent_by = nil)
       output = JSON.pretty_generate(input)
-      if last_line_spacing
-        # Add spacing to the last line if specified
+      if indent_by
         output_lines = output.split("\n")
-        output_lines[-1] = ' ' * last_line_spacing.to_i + output_lines[-1]
-        output_lines.join("\n")
+        indent_spaces = ' ' * indent_by.to_i
+        output_lines.map! { |line| "#{indent_spaces}#{line}" }
+        output = output_lines.join("\n")
       else
         output  # Return the prettified JSON without additional spacing
       end
