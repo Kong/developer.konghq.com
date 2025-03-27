ee609d87988651aaf9a685ff31878e2990607c77
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-03-27 22:01:25 +0100

    Add dynatrace as a plugin (#919)
    * Add dynatrace as a plugin
    
    Idea: Add any page to `/plugins/` by setting `act_as_plugin: true`
    in its frontmatter and the corresponding metadata, we can add those as
    required in the schema if we want to follow this path.
    
    * Sort plugins alphabetically
    
    * Add support for act_as_plugin to how-tos
    
    * switch to using dynatrace how-to
    
    * add act_as_plugin to dictionary
    
    ---------
    
    Co-authored-by: lena-larionova <lena.larionova@konghq.com>
    Co-authored-by: lena-larionova <54370747+lena-larionova@users.noreply.github.com>

--- app/_plugins/hooks/site_pre_render.rb
+++ app/_plugins/hooks/site_pre_render.rb
@@ -3,15 +3,26 @@
 Jekyll::Hooks.register :site, :pre_render do |site|
   site.data['pages_urls'] = Set.new
   site.data['tags'] = Set.new
+  site.data['act_as_plugins'] = {}
 
   site.pages.each do |page|
     site.data['pages_urls'] << page.url if page.data['published'].nil? || page.data['published'] == true
     site.data['tags'].merge(page.data['tags']) if page.data['tags']
+
+    next unless page.data['act_as_plugin']
+
+    slug = page.dir.split('/').last
+    site.data['act_as_plugins'][slug] = page
   end
 
   site.documents.each do |doc|
     site.data['pages_urls'] << doc.url if doc.data['published'].nil? || doc.data['published'] == true
     site.data['tags'].merge(doc.data['tags']) if doc.data['tags']
+
+    next unless doc.data['act_as_plugin']
+
+    slug = doc.basename_without_ext
+    site.data['act_as_plugins'][slug] = doc
   end
 
   site.data['searchFilters'][:tags] = site.data['tags'].to_a.sort.map { |t| { label: t, value: t } }
