6242e2c55e3b6ed6a164d778bfd3847dce0bc8b6
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-04-22 14:29:06 +0200

    Pre-process entries before generating gateway's changelog

--- tools/changelog-generator/run.js
+++ tools/changelog-generator/run.js
@@ -20,12 +20,36 @@ function generateChangelogsByVersion(folderPath, version) {
   });
 
   let changelog = {};
+  const setup = yaml.load(fs.readFileSync("./config/setup.yaml", "utf-8"));
   folders.forEach((folder) => {
     const key = path.basename(folder);
     changelog[key] = [];
     const entries = fastGlob.globSync(`${folder}/*`);
     entries.forEach((entry) => {
-      changelog[key].push(yaml.load(fs.readFileSync(entry, "utf-8")));
+      const change = yaml.load(fs.readFileSync(entry, "utf-8"));
+      if (!change.hasOwnProperty("scope")) {
+        change.scope = setup.defaults.scope;
+      }
+      if (!change.hasOwnProperty("type")) {
+        change.type = setup.defaults.type;
+      }
+      change.type = setup.type_mappings[change.type] || change.type;
+      if (change.scope === "Plugin") {
+        const match = change.message.match(/(\*\*(.*?):?\*\*?)/);
+        if (match && match[2]) {
+          let plugin = match[2].replace(/-plugin/i, "");
+
+          if (setup.skip_plugin_entries.includes(plugin)) {
+            return;
+          }
+          plugin = setup.plugin_mappings[plugin] || plugin;
+          change.message = change.message.replace(
+            /\*\*.*?:?\*\*?/,
+            `**${plugin}**`
+          );
+        }
+      }
+      changelog[key].push(change);
     });
   });
 
