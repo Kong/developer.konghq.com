3827cea1fd58d2f79470021e9f4c3545295e2c3f
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-04-25 12:08:29 +0200

    Add title tags to pages

---
+++ app/_plugins/generators/data/title/reference.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module Jekyll
+  module Data
+    module Title
+      class Reference < Base # rubocop:disable Style/Documentation
+        def title_sections
+          [
+            page_title,
+            version,
+            product_or_tool
+          ]
+        end
+
+        def version
+          return if @page.data['canonical?']
+
+          v = @page.data['release']
+          Gem::Version.correct?(v) ? "v#{v}" : v
+        end
+
+        def product_or_tool
+          if product.nil?
+            @site.data.dig('tools', tool, 'name')
+          else
+            @site.data.dig('products', product, 'name')
+          end
+        end
+
+        def product
+          @product ||= @page.data.fetch('products', []).first
+        end
+
+        def tool
+          @tool ||= @page.data.fetch('tools', []).first
+        end
+      end
+    end
+  end
+end
