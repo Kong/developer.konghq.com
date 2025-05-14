773e7bd9ccb14d729c6ea42798d8061e39692bcc
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-05-14 15:52:00 +0200

    Don't render operator as a tool in the search/how-tos filter

--- app/_plugins/generators/site_data.rb
+++ app/_plugins/generators/site_data.rb
@@ -43,7 +43,7 @@ module Jekyll
     def search_filters(site)
       {
         products: site.data.fetch('products').map { |k, v| { label: v['name'], value: k } },
-        tools: site.data.fetch('tools').except('kic').map { |k, v| { label: v['name'], value: k } },
+        tools: site.data.fetch('tools').except('kic', 'operator').map { |k, v| { label: v['name'], value: k } },
         works_on: site.data.dig('products', 'gateway', 'deployment_topologies').map do |t|
           { label: t['text'], value: t['slug'] }
         end
