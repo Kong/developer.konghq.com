149856cf30bd859eeb0745caae06a1ba16f321bd
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-06-06 13:32:33 +0200

    Add {% env_variables %} block for setting env variables

---
+++ app/_plugins/drops/validations/env_variables.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'json'
+require_relative './base'
+
+module Jekyll
+  module Drops
+    module Validations
+      class EnvVariables < Base # rubocop:disable Style/Documentation
+        def variables
+          @variables ||= @yaml
+        end
+
+        def data_validate
+          JSON.dump(variables)
+        end
+
+        def validate_yaml!
+          raise ArgumentError, "Missing variables in {% validation #{id} %}." if @yaml.empty?
+        end
+      end
+    end
+  end
+end
