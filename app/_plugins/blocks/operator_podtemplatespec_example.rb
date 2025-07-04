68de09914704578bc418562fad0ee089cfd1f213
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-07-04 17:04:00 -0300

    Remove extra spacing in {% operator_podtemplatespec_example %}

--- app/_plugins/blocks/operator_podtemplatespec_example.rb
+++ app/_plugins/blocks/operator_podtemplatespec_example.rb
@@ -11,36 +11,19 @@ module Jekyll
         raise "Unable to parse config in operator_podtemplatespec_example: \n#{spec}\n\n#{e}"
       end
 
-      dp = config['dataplane'].to_yaml.split("\n")[1..].join("\n")
+      return '' unless config['dataplane']
 
-      <<~TEXT
-        #{dataplane_example(dp, config['kubectl_apply']) if config['dataplane']}#{' '}
-      TEXT
+      context.stack do
+        context['kubectl_apply'] = config['kubectl_apply']
+        context['spec'] = Jekyll::Utils::HashToYAML.new(config['dataplane']).convert
+        Liquid::Template.parse(template).render(context)
+      end
     end
 
     private
 
-    def dataplane_example(spec, kubectl_apply) # rubocop:disable Metrics/MethodLength
-      return '' if spec.empty?
-
-      <<~TEXT
-        {:.info}
-        > The following example uses the `DataPlane` resource, but you can also configure your `GatewayConfiguration` resource as needed. For more information see the [PodTemplateSpec](/operator/dataplanes/reference/podtemplatespec/) page.
-
-        ```yaml
-        #{kubectl_apply && "echo '"}
-        apiVersion: gateway-operator.konghq.com/v1beta1
-        kind: DataPlane
-        metadata:
-          name: dataplane-example
-          namespace: kong
-        spec:
-          deployment:
-            podTemplateSpec:
-              #{spec.split("\n").map { |line| "      #{line}" }.join("\n").strip}
-        #{kubectl_apply && "' | kubectl apply -f -"}
-        ```
-      TEXT
+    def template
+      @template ||= File.read(File.expand_path('app/_includes/components/operator_podtemplatespec_example.md'))
     end
   end
 end
