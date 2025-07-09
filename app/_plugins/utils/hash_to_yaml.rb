593e20867b7b0fc1ec70864d07eec5c3be833d9e
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-07-09 17:58:09 -0300

    Fix indentation issues

--- app/_plugins/utils/hash_to_yaml.rb
+++ app/_plugins/utils/hash_to_yaml.rb
@@ -9,18 +9,18 @@ module Jekyll
         @hash = hash
       end
 
-      def convert
-        indent_yaml(YAML.dump(@hash).delete_prefix("---\n").chomp)
+      def convert(indent_level: 2)
+        indent_yaml(YAML.dump(@hash).delete_prefix("---\n").chomp, indent_level)
       end
 
-      def indent_yaml(yaml_string, indent_level = 2)
+      def indent_yaml(yaml_string, indent_level)
         indented_yaml = []
         yaml_string.lines.each do |line|
-          if line.match?(/^(\s*)-\s+/)
-            indented_yaml << ' ' * indent_level + line
-          else
-            indented_yaml << line.gsub(/^(\s+)/) { |spaces| ' ' * (spaces.length + indent_level) }
-          end
+          indented_yaml << if line.match?(/^(\s*)-\s+/)
+                             ' ' * indent_level + line
+                           else
+                             line.gsub(/^(\s+)/) { |spaces| ' ' * (spaces.length + indent_level) }
+                           end
         end
         indented_yaml.join
       end
