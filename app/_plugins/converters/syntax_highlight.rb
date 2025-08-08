fb10e5dc6a6eed35eac26fd1b91c4f0dabb30ca7
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-08-08 13:48:47 +0200

    feat: add basic syntax highlighting with https://andreruffert.github.io/syntax-highlight-element/ and disable kramdown/rouge

---
+++ app/_plugins/converters/syntax_highlight.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'kramdown/parser/kramdown'
+require 'kramdown/converter/html'
+
+module Kramdown
+  module Converter
+    class Html
+      alias original_convert_codeblock convert_codeblock
+
+      def convert_codeblock(el, _indent) # rubocop:disable Naming/MethodParameterName,Metrics/MethodLength
+        attr = el.attr.dup
+        data = data_attributes(attr)
+        copy = copy?(attr)
+
+        Liquid::Template.parse(template).render(
+          { 'include' => {
+            'copy' => copy,
+            'data' => data, 'lang' => lang(attr), 'code' => escape_html(el.value),
+            'css_classes' => attr['class'],
+            'render_header' => data['data-file'] || copy
+          } },
+          context
+        )
+      end
+
+      def data_attributes(attr)
+        @data_attributes = attr.each_with_object({}) do |(key, value), data|
+          data[key] = value if key.start_with?('data-')
+        end
+      end
+
+      def lang(attr)
+        extract_code_language!(attr) || 'text'
+      end
+
+      def copy?(attr)
+        !attr.fetch('class', '').include?('no-copy-code')
+      end
+
+      def site
+        @site ||= Jekyll.sites.first
+      end
+
+      def context
+        @context = Liquid::Context.new(site, {}, {})
+      end
+
+      def template
+        @template ||= File.read(File.expand_path('app/_includes/syntax_highlighting.html'))
+      end
+    end
+  end
+end
