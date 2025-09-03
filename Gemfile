31a19b3ccf345fa890c98c3a8977bac5807061e3
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-09-03 15:54:49 +0200

    feat: Syntax highlighting (#2548)
    * feat: Add syntax-highlight-element basic dark/light mode handling and
    disable kramdown's syntax highlighting
    
    * feat: add basic syntax highlighting with https://andreruffert.github.io/syntax-highlight-element/
    and disable kramdown/rouge
    
    * feat: replace custom copy to clipboard with https://github.com/github/clipboard-copy-element
    
    * chore: Fix singletick code blocks
    
    * chore(automated-tests): Replace puppeteer with playwright
    
    * chore: wip override some syntax highlighting colors
    
    * refactor(codeblocks): refactor template
    
    * chore: change hover styles of copy button
    
    * refactor: remove syntax-highlight and add shiki
    
    * use rouge in dev
    
    * feat: use shiki for syntax highlighting in preview and production envs
    
    * fix: set data attributes on code snippets
    
    * Add nodo as a dependency
    
    * use nodo for running shiki
    
    Cache the highlighter instance in a script and defined a highlight
    function to generate the html
    
    * Use samy background that the code-snippets in the spec-renderer have in
    light mode
    
    * change the way we import shiki
    
    * Use shiki in dev too
    
    * Add `rego` as supported language to shiki
    Rego isn't supported, so falling back to text
    
    * change code snippets bg color in dark mode
    
    * fix: add `bash` as language to gateway prereq
    
    * Add more styles top code-block header
    
    * change bg color for codeblocks in lightmode
    
    * update background colors in code snippets on API pages

--- Gemfile
+++ Gemfile
@@ -1,25 +1,25 @@
 source 'https://rubygems.org'
 
+gem 'activesupport'
+gem 'csv'
 gem 'jekyll'
 gem 'jekyll-include-cache'
+gem 'jekyll-vite'
 gem 'kramdown-parser-gfm'
 gem 'liquid-c'
-gem 'rouge', '~> 4.3'
-gem 'jekyll-vite'
-gem 'activesupport'
+gem 'nodo'
 gem 'nokogiri'
-gem 'csv'
-
+gem 'rouge', '~> 4.3'
 # XXX: bundler isn't installing mini_portile as a dependency of nokogiri
 # installing it manually fixes the issue
 gem 'mini_portile2', '~> 2.8'
 
 group :development do
+  gem 'byebug'
+  gem 'foreman'
   gem 'pry'
   gem 'puma'
   gem 'rubocop'
-  gem 'foreman'
-  gem 'byebug'
 end
 
 group :jekyll_plugins do
---
+++ app/_plugins/converters/shiki.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'nodo'
+
+class CodeHighlighter < Nodo::Core # rubocop:disable Style/Documentation
+  require :shiki
+
+  script do
+    <<~JS
+      const highlighter = await shiki.createHighlighter({
+        themes: ["catppuccin-latte", "catppuccin-mocha"],
+        langs: [
+          "yaml",
+          "fish",
+          "json",
+          "lua",
+          "http",
+          "python",
+          "hcl",
+          "javascript",
+          "sql",
+          "dockerfile",
+          "go",
+          "terraform",
+          "nginx",
+          "html",
+          "ruby"
+        ],
+      });
+    JS
+  end
+
+  function :highlight, <<~JS
+    async (code, lang, id) => {
+      const languageMap = {
+          bash: "fish",
+          sh: "fish",
+          console: "fish",
+          cef: "text",
+          conf: "yaml",
+          rego: "text"
+      };
+      const language = languageMap[lang] || lang;
+      const html = highlighter.codeToHtml(code, {
+        lang: language,
+        themes: {
+          light: "catppuccin-latte",
+          dark: "catppuccin-mocha",
+        },
+        transformers: [
+          {
+            code(node) {
+              node.properties.id = id;
+            },
+          },
+        ],
+      });
+      return html;
+    }
+  JS
+end
