b563fe0e965d9cee6845baa391c00f02a0316e35
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-09-12 08:37:02 +0200

    feat: use a toggle switch for the deployment topology
    Works in the same way as the dropdown, i.e. sets the value in the
    following order (each option overwrites the previous value):
    * user preference stored.
    * "?platform" param in the url.
    * if the page has data-topology-switcher="page" updates the url
      depending on the value of the switch.

---
+++ app/_assets/javascripts/components/switch.js
@@ -0,0 +1,145 @@
+class ToggleSwitch {
+  constructor(elem) {
+    this.elem = elem;
+    this.inputs = this.elem.querySelectorAll(".switch__input");
+    this.onPrem = this.elem.querySelector('label[for*="on-prem"]');
+    this.konnect = this.elem.querySelector('label[for*="konnect"]');
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.inputs.forEach((radio) => {
+      radio.addEventListener("change", this.onToggleChange.bind(this));
+    });
+  }
+
+  onToggleChange(event) {
+    const selectedValue = event.target.value;
+    const changeEvent = new CustomEvent("switch-state-change", {
+      detail: { selectedValue },
+    });
+
+    document.dispatchEvent(changeEvent);
+  }
+
+  updateState(selectedValue) {
+    this.inputs.forEach((radio) => {
+      radio.checked = radio.value === selectedValue;
+    });
+  }
+}
+
+export default class ToggleSwitchManager {
+  constructor() {
+    this.deploymentToplogyKey = "deployment-topology-switch";
+    this.type = "toggle";
+    this.values = ["konnect", "on-prem"];
+    this.switches = [];
+    this.initialValue = "konnect";
+
+    document.querySelectorAll(".switch").forEach((elem) => {
+      this.switches.push(new ToggleSwitch(elem));
+    });
+
+    this.topologySwitcherOption = document.querySelector(
+      "[data-topology-switcher]"
+    );
+
+    this.addEventListeners();
+    this.init();
+  }
+
+  init() {
+    if (this.switches.length > 0) {
+      this.setType();
+      this.setInitialValue();
+    }
+  }
+
+  setInitialValue() {
+    if (this.getStoredValue()) {
+      this.initialValue = this.getStoredValue();
+    }
+    if (this.getParamValue()) {
+      this.initialValue = this.getParamValue();
+    }
+    if (this.getURLValue()) {
+      this.initialValue = this.getURLValue();
+    }
+
+    const initialEvent = new CustomEvent("switch-state-change", {
+      detail: { selectedValue: this.initialValue },
+    });
+    document.dispatchEvent(initialEvent);
+  }
+
+  setType() {
+    if (this.topologySwitcherOption) {
+      this.type = this.topologySwitcherOption.dataset.topologySwitcher;
+    }
+  }
+
+  getURLValue() {
+    if (this.type === "page") {
+      if (this.values.includes(this.lastURLSegment())) {
+        return this.lastURLSegment();
+      }
+    }
+  }
+
+  lastURLSegment() {
+    const segments = window.location.pathname.split("/");
+    if (!segments[segments.length - 1]) {
+      segments.pop();
+    }
+    return segments[segments.length - 1];
+  }
+
+  switchPageTopology(value) {
+    if (this.lastURLSegment() !== value) {
+      const url = new URL(window.location.href);
+      const segments = url.pathname.split("/");
+      if (!segments[segments.length - 1]) {
+        segments.pop();
+      }
+      segments[segments.length - 1] = value;
+      url.pathname = segments.join("/");
+      window.location.href = url.toString();
+    }
+  }
+
+  getStoredValue() {
+    try {
+      if (localStorage.getItem(this.deploymentToplogyKey) !== null) {
+        const storedValue = localStorage.getItem(this.deploymentToplogyKey);
+        if (storedValue && this.values.some((v) => v === storedValue)) {
+          return storedValue;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getParamValue() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const deployment = urlParams.get("deployment");
+    if (this.values.includes(deployment)) {
+      return deployment;
+    }
+  }
+
+  addEventListeners() {
+    document.addEventListener("switch-state-change", (event) => {
+      const { selectedValue } = event.detail;
+      localStorage.setItem(this.deploymentToplogyKey, selectedValue);
+
+      if (this.type === "page") {
+        this.switchPageTopology(selectedValue);
+      }
+      this.switches.forEach((instance) => {
+        instance.updateState(selectedValue);
+      });
+    });
+  }
+}
