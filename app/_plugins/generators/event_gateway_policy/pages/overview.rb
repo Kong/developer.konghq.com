e98763e96f9860d8faa564ef5061636c6e7c28cd
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2025-10-07 10:45:28 +0200

    feat(event-gateway): add policies hub

--- app/_plugins/generators/event_gateway_policy/pages/overview.rb
+++ app/_plugins/generators/event_gateway_policy/pages/overview.rb
@@ -7,6 +7,13 @@ module Jekyll
     module Pages
       class Overview < Base
         include Policies::Pages::Overview
+
+        def data
+          super.merge(
+            'phases' => @policy.phases || [],
+            'policy_target' => @policy.policy_target
+          )
+        end
       end
     end
   end
