15e08a95069d0ac7dac20bc41a08ac354c40c941
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-02-03 15:43:07 +0100

    feat(llms): add new_in

--- app/_plugins/tags/new_in.rb
+++ app/_plugins/tags/new_in.rb
@@ -11,6 +11,7 @@ module Jekyll
     def render(context)
       @context = context
       @site = context.registers[:site]
+      @page = @context.environments.first['page']
 
       raise ArgumentError, 'Missing required parameter `version` for {% new_in %} ' unless @param
 
@@ -25,7 +26,11 @@ module Jekyll
     private
 
     def template
-      @template ||= File.read(File.expand_path('app/_includes/components/new_in.html'))
+      @template ||= if @page['output_format'] == 'markdown'
+                      File.read(File.expand_path('app/_includes/components/new_in.md'))
+                    else
+                      File.read(File.expand_path('app/_includes/components/new_in.html'))
+                    end
     end
   end
 end
