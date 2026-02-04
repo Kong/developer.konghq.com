927605cbc9fae1c920e24305fc53302b771e2ce0
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-02-04 12:32:06 +0100

    feat(llms): add entity_schema

--- app/_plugins/tags/entity_schema.rb
+++ app/_plugins/tags/entity_schema.rb
@@ -4,25 +4,29 @@ module Jekyll
   class RenderEntitySchema < Liquid::Tag
     def render(context)
       @context = context
-      page = context.environments.first['page']
+      @page = context.environments.first['page']
       site = context.registers[:site]
-      release = page['release']
-      schema = page['schema']
+      release = @page['release']
+      schema = @page['schema']
 
-      if schema
-        entity_schema_drop = Drops::EntitySchema.new(schema:, site:, release:)
+      return unless schema
 
-        context.stack do
-          context['entity_schema'] = entity_schema_drop
-          Liquid::Template.parse(template).render(context)
-        end
+      entity_schema_drop = Drops::EntitySchema.new(schema:, site:, release:)
+
+      context.stack do
+        context['entity_schema'] = entity_schema_drop
+        Liquid::Template.parse(template).render(context)
       end
     end
 
     private
 
     def template
-      @template ||= File.read(File.expand_path('app/_includes/components/entity_schema.html'))
+      @template ||= if @page['output_format'] == 'markdown'
+                      File.read(File.expand_path('app/_includes/components/entity_schema.md'))
+                    else
+                      File.read(File.expand_path('app/_includes/components/entity_schema.html'))
+                    end
     end
   end
 end
