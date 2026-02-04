3937ddfca3557495b11e047bb521b11ffd77836b
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-02-04 16:51:02 +0100

    feat(llms): add konnect_api_request

--- app/_plugins/blocks/konnect_api_request.rb
+++ app/_plugins/blocks/konnect_api_request.rb
@@ -13,17 +13,17 @@ module Jekyll
       @context = context
       @site = context.registers[:site]
       @page = context.environments.first['page']
+      @format = @page['output_format'] || 'html'
 
       contents = super
 
-      unless @page.fetch('works_on', []).any? { |w| ['konnect', 'konnect-platform'].include?(w) }
+      unless @page.fetch('works_on', []).any? { |w| %w[konnect konnect-platform].include?(w) }
         raise ArgumentError,
               'Page does not contain works_on: konnect or konnect-platform, but uses {% konnect_api_request %}'
       end
-      
 
       config = YAML.load(contents)
-      drop = Drops::KonnectApiRequest.new(yaml: config)
+      drop = Drops::KonnectApiRequest.new(yaml: config, format: @format)
 
       output = context.stack do
         context['config'] = drop
