ab58468de394a58b5648c6a637f1a2ce9e1fbac9
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-03-17 16:26:06 +0100

    wip(ai-cookbook): add basic ai-cookbook based on a recipe

--- app/_plugins/generators/data/search_tags/base.rb
+++ app/_plugins/generators/data/search_tags/base.rb
@@ -13,7 +13,8 @@ module Jekyll
           'plugin_example' => 'PluginExample',
           'reference' => 'Reference',
           'policy' => 'Policy',
-          'support' => 'Support'
+          'support' => 'Support',
+          'ai_cookbook' => 'Reference'
         }.freeze
 
         def self.make_for(site:, page:)
