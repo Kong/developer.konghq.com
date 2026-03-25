1b622226a7d3f137f909e95cee2691f354392116
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-03-25 16:36:12 +0100

    further improvements

--- app/_plugins/generators/event_gateway_policies.rb
+++ app/_plugins/generators/event_gateway_policies.rb
@@ -4,27 +4,40 @@ module Jekyll
   class EventGatewayPoliciesGenerator < Jekyll::Generator # rubocop:disable Style/Documentation
     priority :high
 
-    def generate(site)
-      current_mtimes = Utils::Incremental.collect_mtimes(
-        File.join(site.source, '_event_gateway_policies/**/*')
-      )
+    POLICIES_FOLDER = '_event_gateway_policies'
 
-      if Utils::Incremental.enabled? && @cached_mtimes && @cached_pages && !Utils::Incremental.mtimes_changed?(current_mtimes, @cached_mtimes)
-        site.pages.concat(@cached_pages)
-        Utils::Incremental.skip_regeneration(site, @cached_pages)
-        site.data['event_gateway_policies'] = @cached_event_gateway_policies
-        Jekyll.logger.info 'IncrementalGen:', 'Skipped EventGatewayPoliciesGenerator (sources unchanged)'
+    def generate(site) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      site.data['event_gateway_policies'] ||= {}
+
+      unless Utils::Incremental.enabled?
+        Jekyll::EventGatewayPolicyPages::Generator.run(site)
         return
       end
 
-      site.data['event_gateway_policies'] ||= {}
+      @policy_cache ||= {}
+      skipped = 0
+      generator = Jekyll::EventGatewayPolicyPages::Generator.new(site)
 
-      before = site.pages.size
-      Jekyll::EventGatewayPolicyPages::Generator.run(site)
+      Dir.glob(File.join(site.source, "#{POLICIES_FOLDER}/*/")).each do |folder|
+        slug = folder.gsub("#{site.source}/#{POLICIES_FOLDER}/", '').chomp('/')
+        current_mtimes = Utils::Incremental.collect_mtimes("#{folder}**/*")
+        cached = @policy_cache[slug]
 
-      @cached_pages = site.pages[before..]
-      @cached_event_gateway_policies = site.data['event_gateway_policies'].dup
-      @cached_mtimes = current_mtimes
+        if cached && !Utils::Incremental.mtimes_changed?(current_mtimes, cached[:mtimes])
+          site.pages.concat(cached[:pages])
+          Utils::Incremental.skip_regeneration(site, cached[:pages])
+          site.data['event_gateway_policies'][slug] = cached[:data]
+          skipped += 1
+        else
+          before = site.pages.size
+          policy = Jekyll::EventGatewayPolicyPages::Policy.new(folder:, slug:)
+          generator.generate_pages(policy)
+          new_pages = site.pages[before..]
+          @policy_cache[slug] = { mtimes: current_mtimes, pages: new_pages, data: site.data['event_gateway_policies'][slug] }
+        end
+      end
+
+      Jekyll.logger.info 'IncrementalGen:', "EventGatewayPoliciesGenerator: #{skipped} policies restored from cache" if skipped.positive?
     end
   end
 end
