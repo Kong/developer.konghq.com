2f2e02d97ccd024934d43f5d37e8b8c67586eddc
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-03-25 16:36:12 +0100

    further improvements

--- app/_plugins/generators/mesh_policies.rb
+++ app/_plugins/generators/mesh_policies.rb
@@ -4,28 +4,47 @@ module Jekyll
   class MeshPoliciesGenerator < Jekyll::Generator # rubocop:disable Style/Documentation
     priority :high
 
-    def generate(site)
-      current_mtimes = Utils::Incremental.collect_mtimes(
-        File.join(site.source, '_mesh_policies/**/*'),
-        File.join(site.config['mesh_policy_schemas_path'], '**/*')
-      )
+    POLICIES_FOLDER = '_mesh_policies'
 
-      if Utils::Incremental.enabled? && @cached_mtimes && @cached_pages && !Utils::Incremental.mtimes_changed?(current_mtimes, @cached_mtimes)
-        site.pages.concat(@cached_pages)
-        Utils::Incremental.skip_regeneration(site, @cached_pages)
-        site.data['mesh_policies'] = @cached_mesh_policies
-        Jekyll.logger.info 'IncrementalGen:', 'Skipped MeshPoliciesGenerator (sources unchanged)'
+    def generate(site) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      site.data['mesh_policies'] ||= {}
+
+      unless Utils::Incremental.enabled?
+        Jekyll::MeshPolicyPages::Generator.run(site)
         return
       end
 
-      site.data['mesh_policies'] ||= {}
+      @policy_cache ||= {}
+      schema_mtimes = Utils::Incremental.collect_mtimes(
+        File.join(site.config['mesh_policy_schemas_path'], '**/*')
+      )
+      schemas_changed = @cached_schema_mtimes.nil? || Utils::Incremental.mtimes_changed?(schema_mtimes, @cached_schema_mtimes)
+      @policy_cache.clear if schemas_changed
 
-      before = site.pages.size
-      Jekyll::MeshPolicyPages::Generator.run(site)
+      skipped = 0
+      generator = Jekyll::MeshPolicyPages::Generator.new(site)
 
-      @cached_pages = site.pages[before..]
-      @cached_mesh_policies = site.data['mesh_policies'].dup
-      @cached_mtimes = current_mtimes
+      Dir.glob(File.join(site.source, "#{POLICIES_FOLDER}/*/")).each do |folder|
+        slug = folder.gsub("#{site.source}/#{POLICIES_FOLDER}/", '').chomp('/')
+        current_mtimes = Utils::Incremental.collect_mtimes("#{folder}**/*")
+        cached = @policy_cache[slug]
+
+        if cached && !Utils::Incremental.mtimes_changed?(current_mtimes, cached[:mtimes])
+          site.pages.concat(cached[:pages])
+          Utils::Incremental.skip_regeneration(site, cached[:pages])
+          site.data['mesh_policies'][slug] = cached[:data]
+          skipped += 1
+        else
+          before = site.pages.size
+          policy = Jekyll::MeshPolicyPages::Policy.new(folder:, slug:)
+          generator.generate_pages(policy)
+          new_pages = site.pages[before..]
+          @policy_cache[slug] = { mtimes: current_mtimes, pages: new_pages, data: site.data['mesh_policies'][slug] }
+        end
+      end
+
+      @cached_schema_mtimes = schema_mtimes
+      Jekyll.logger.info 'IncrementalGen:', "MeshPoliciesGenerator: #{skipped} policies restored from cache" if skipped.positive?
     end
   end
 end
--- app/_plugins/generators/plugins.rb
+++ app/_plugins/generators/plugins.rb
@@ -4,28 +4,47 @@ module Jekyll
   class PluginsGenerator < Jekyll::Generator
     priority :high
 
-    def generate(site)
-      current_mtimes = Utils::Incremental.collect_mtimes(
-        File.join(site.source, '_kong_plugins/**/*'),
-        File.join(site.config['plugin_schemas_path'], '**/*')
-      )
+    PLUGINS_FOLDER = '_kong_plugins'
 
-      if Utils::Incremental.enabled? && @cached_mtimes && @cached_pages && !Utils::Incremental.mtimes_changed?(current_mtimes, @cached_mtimes)
-        site.pages.concat(@cached_pages)
-        Utils::Incremental.skip_regeneration(site, @cached_pages)
-        site.data['kong_plugins'] = @cached_kong_plugins
-        Jekyll.logger.info 'IncrementalGen:', 'Skipped PluginsGenerator (sources unchanged)'
+    def generate(site) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      site.data['kong_plugins'] ||= {}
+
+      unless Utils::Incremental.enabled?
+        Jekyll::PluginPages::Generator.run(site)
         return
       end
 
-      site.data['kong_plugins'] ||= {}
+      @plugin_cache ||= {}
+      schema_mtimes = Utils::Incremental.collect_mtimes(
+        File.join(site.config['plugin_schemas_path'], '**/*')
+      )
+      schemas_changed = @cached_schema_mtimes.nil? || Utils::Incremental.mtimes_changed?(schema_mtimes, @cached_schema_mtimes)
+      @plugin_cache.clear if schemas_changed
 
-      before = site.pages.size
-      Jekyll::PluginPages::Generator.run(site)
+      skipped = 0
+      generator = Jekyll::PluginPages::Generator.new(site)
 
-      @cached_pages = site.pages[before..]
-      @cached_kong_plugins = site.data['kong_plugins'].dup
-      @cached_mtimes = current_mtimes
+      Dir.glob(File.join(site.source, "#{PLUGINS_FOLDER}/*/")).each do |folder|
+        slug = folder.gsub("#{site.source}/#{PLUGINS_FOLDER}/", '').chomp('/')
+        current_mtimes = Utils::Incremental.collect_mtimes("#{folder}**/*")
+        cached = @plugin_cache[slug]
+
+        if cached && !Utils::Incremental.mtimes_changed?(current_mtimes, cached[:mtimes])
+          site.pages.concat(cached[:pages])
+          Utils::Incremental.skip_regeneration(site, cached[:pages])
+          site.data['kong_plugins'][slug] = cached[:data]
+          skipped += 1
+        else
+          before = site.pages.size
+          plugin = Jekyll::PluginPages::Plugin.new(folder:, slug:)
+          generator.generate_pages(plugin)
+          new_pages = site.pages[before..]
+          @plugin_cache[slug] = { mtimes: current_mtimes, pages: new_pages, data: site.data['kong_plugins'][slug] }
+        end
+      end
+
+      @cached_schema_mtimes = schema_mtimes
+      Jekyll.logger.info 'IncrementalGen:', "PluginsGenerator: #{skipped} plugins restored from cache" if skipped.positive?
     end
   end
 end
