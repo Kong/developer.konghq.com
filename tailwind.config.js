01829e68aec15e366bd02221d9cbd7bba3c509c8
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-04-15 09:08:40 +0200

    feat(cookbooks): add index page for cookbooks

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
     "app/_how-tos/**/*.md",
     "app/_plugins/**/*.rb",
     "app/_assets/javascripts/**",
+    "app/_assets/entrypoints/**",
     "app/gateway/**",
+    "app/cookbooks/**/*.html",
   ],
   darkMode: "selector",
   safelist: [
