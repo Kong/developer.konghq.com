e3d94f4db9da74daf248537e9ae82006a0549565
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-06-24 16:45:34 +0200

    feat(automated-tests): update extractor so that aigw v1 means gateway so we run both aigw v1 and gateway tests as part of the same run

--- tools/automated-tests/instructions/extractor.js
+++ tools/automated-tests/instructions/extractor.js
@@ -182,12 +182,14 @@ function deriveProduct(setup, products) {
   // String value
   if (setupEntry === "konnect") {
     // For konnect, determine the product from the products list
-    if (products.includes("ai-gateway")) {
-      return "ai-gateway";
-    }
-    if (products.includes("event-gateway")) {
-      return "event-gateway";
-    }
+    // Special case for ai-gateway v1
+    if (products.includes("ai-gateway") && products.includes("gateway")) {
+      return "gateway";
+    } else if (products.includes("ai-gateway")) {
+        return "ai-gateway";
+    } else if (products.includes("event-gateway")) {
+        return "event-gateway";
+      }
     return "gateway";
   }
   // e.g., "operator"
