b7b116897d233060f41bb981747e32917010f4ef
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-06-29 13:18:06 -0300

    Feat/aigw changelog (#5722)
    * refactor(gw-changelog): add specs
    
    * refactor(gw-changelog): move changelogs from app/_data to
    app/_changelogs to reduce the memory footprint
    
    * refactor(gw-changelog): update tools to match the new paths
    
    * feat(aigw-changelog): update changelog-generator tool to generate both
    aigw and gw changelogs
    
    * feat(changelogs): add the new files to the right path, i.e.
    app/_changelogs
    
    * feat(aigw-changelog): update the {% gateway_changelog %} tag to support
    both gateway and ai-gateway
    
    It chooses which one to render based on the page's `product.first`
    
    * feat(aigw-changelog): add aigw changelog page
    
    * fix(aigw-changelog): no need to use a multiline string for the
    decsription

--- tools/changelog-generator/changelog.js
+++ tools/changelog-generator/changelog.js
@@ -3,6 +3,7 @@ import yaml from "js-yaml";
 import path from "path";
 import { globSync } from "tinyglobby";
 import mergeWith from "lodash.mergewith";
+import minimist from "minimist";
 import { compareVersions } from "./compare-versions.js";
 
 function customMerge(objValue, srcValue) {
@@ -11,18 +12,20 @@ function customMerge(objValue, srcValue) {
   }
 }
 
-function generateChangelog() {
+function generateChangelog(product) {
   try {
-    const changelogFilePath = `../../app/_data/changelogs/gateway.json`;
+    const changelogFilePath = `../../app/_changelogs/${product}.json`;
     let changelog = {};
     if (fs.existsSync(changelogFilePath)) {
       changelog = JSON.parse(fs.readFileSync(changelogFilePath, "utf-8"));
     }
 
-    let changelogByVersion = globSync(`./tmp/*`);
-    changelogByVersion = changelogByVersion.concat(
-      globSync(`./missing_changelogs/*`)
-    );
+    let changelogByVersion = globSync(`./tmp/${product}/*`);
+    if (product === "gateway") {
+      changelogByVersion = changelogByVersion.concat(
+        globSync(`./missing_changelogs/*`)
+      );
+    }
     const orderedFiles = changelogByVersion.sort(compareVersions).reverse();
 
     // changelog files
@@ -42,45 +45,47 @@ function generateChangelog() {
       }
     });
 
-    // missing entries
-    const entries_by_version = {};
-    const baseDir = "./missing_entries";
-    const versionDirs = fs.readdirSync(baseDir, { withFileTypes: true });
+    // missing entries (gateway only)
+    if (product === "gateway") {
+      const entries_by_version = {};
+      const baseDir = "./missing_entries";
+      const versionDirs = fs.readdirSync(baseDir, { withFileTypes: true });
 
-    versionDirs.forEach((dirent) => {
-      if (dirent.isDirectory()) {
-        const version = dirent.name;
-        const versionPath = path.join(baseDir, version);
+      versionDirs.forEach((dirent) => {
+        if (dirent.isDirectory()) {
+          const version = dirent.name;
+          const versionPath = path.join(baseDir, version);
 
-        const files = fs
-          .readdirSync(versionPath)
-          .map((file) => "./" + path.join(versionPath, file));
+          const files = fs
+            .readdirSync(versionPath)
+            .map((file) => "./" + path.join(versionPath, file));
 
-        const entries = files.flatMap((f) =>
-          yaml.load(fs.readFileSync(f, "utf-8"))
-        );
-        entries_by_version[version] = entries;
-      }
-    });
+          const entries = files.flatMap((f) =>
+            yaml.load(fs.readFileSync(f, "utf-8"))
+          );
+          entries_by_version[version] = entries;
+        }
+      });
 
-    for (const version in entries_by_version) {
-      if (changelog[version]) {
-        changelog[version] = mergeWith(
-          {},
-          changelog[version],
-          { "kong-ee": entries_by_version[version] },
-          customMerge
-        );
-      } else {
-        changelog[version] = { "kong-ee": entries_by_version[version] };
+      for (const version in entries_by_version) {
+        if (changelog[version]) {
+          changelog[version] = mergeWith(
+            {},
+            changelog[version],
+            { "kong-ee": entries_by_version[version] },
+            customMerge
+          );
+        } else {
+          changelog[version] = { "kong-ee": entries_by_version[version] };
+        }
       }
     }
 
     // remove duplicate entries...
     for (const version in changelog) {
-      for (const product in changelog[version]) {
-        changelog[version][product] = Object.values(
-          changelog[version][product].reduce((acc, obj) => {
+      for (const component in changelog[version]) {
+        changelog[version][component] = Object.values(
+          changelog[version][component].reduce((acc, obj) => {
             acc[obj.message] = obj;
             return acc;
           }, {})
@@ -101,6 +106,14 @@ function generateChangelog() {
 }
 
 (function main() {
-  console.log("Generating gateway's changelog.json...");
-  generateChangelog();
+  const args = minimist(process.argv.slice(2), { string: ["product"] });
+  const product = args.product || "gateway";
+
+  if (!['gateway', 'ai-gateway'].includes(product)) {
+    console.error(`Unknown --product "${product}": must be "gateway" or "ai-gateway"`);
+    process.exit(1);
+  }
+
+  console.log(`Generating ${product}'s changelog.json...`);
+  generateChangelog(product);
 })();
