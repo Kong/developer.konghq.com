65c379c811e998b592389dfd1ed4188f926e89d3
lena-larionova <54370747+lena-larionova@users.noreply.github.com>
2026-07-15 15:22:15 -0700

    fix typo

--- app/_plugins/generators/api_pages/index.rb
+++ app/_plugins/generators/api_pages/index.rb
@@ -30,7 +30,7 @@ module Jekyll
       def data
         {
           'title' => 'API Reference',
-          'description' => 'Directory of API references documentation for various Kong APIs.',
+          'description' => 'Directory of API references for various Kong APIs.',
           'layout' => 'api/index',
           'get_help' => false,
           'edit_and_issue_links' => false,
