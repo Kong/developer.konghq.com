e434761c5f34a302ef279d4cbecdef06cd89e826
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-09-16 17:23:50 +0200

    feat(drops): build the snippet config per topology in ControlPlaneRequest

--- app/_plugins/drops/control_plane_request.rb
+++ app/_plugins/drops/control_plane_request.rb
@@ -2,11 +2,13 @@
 
 require 'json'
 require_relative '../lib/site_accessor'
+require_relative './concerns/request_snippet_config'
 
 module Jekyll
   module Drops
     class ControlPlaneRequest < Liquid::Drop # rubocop:disable Style/Documentation
       include Jekyll::SiteAccessor
+      include Jekyll::Drops::Concerns::RequestSnippetConfig
 
       def initialize(yaml:, format:) # rubocop:disable Lint/MissingSuper
         @yaml = yaml
@@ -40,6 +42,14 @@ module Jekyll
         @on_prem_url ||= File.join(
           base_url, @yaml['url']
         ).to_s
+      end
+
+      def konnect_snippet_config
+        @konnect_snippet_config ||= snippet_config_for(konnect_url)
+      end
+
+      def on_prem_snippet_config
+        @on_prem_snippet_config ||= snippet_config_for(on_prem_url)
       end
 
       def data_validate_konnect
