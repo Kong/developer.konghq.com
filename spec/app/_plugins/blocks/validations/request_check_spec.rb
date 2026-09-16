85e4f4aa9362ed3f3531dc82325a297e6d7b7ec0
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-09-16 14:49:19 +0200

    fix(request-check): pass all the options to the template and make the html and md version match

--- spec/app/_plugins/blocks/validations/request_check_spec.rb
+++ spec/app/_plugins/blocks/validations/request_check_spec.rb
@@ -172,22 +172,30 @@ RSpec.describe Jekyll::Validation do
     subject(:template_source) { File.read('app/_includes/how-tos/validations/request-check/index.html') }
 
     it 'renders the snippet include for konnect' do
-      expect(template_source).to include('{% include how-tos/validations/request-check/snippet.md url=config.konnect_url')
+      expect(template_source).to include(
+        '{% include how-tos/validations/request-check/snippet.md url=config.konnect_url headers=config.headers form_data=config.form_data form_url_encoded_data=config.form_url_encoded_data body_file=config.body_file body=config.body body_cmd=config.body_cmd method=config.method user=config.user sleep=config.sleep inline_sleep=config.inline_sleep display_headers=config.display_headers cookie_jar=config.cookie_jar cookie=config.cookie message=config.message mtls=config.mtls count=config.count insecure=config.insecure expected_headers=config.expected_headers output=config.output capture=config.capture jq=config.jq %}' # rubocop:disable Layout/LineLength
+      )
     end
 
     it 'renders the snippet include for on-prem' do
-      expect(template_source).to include('{% include how-tos/validations/request-check/snippet.md url=config.on_prem_url')
+      expect(template_source).to include(
+        '{% include how-tos/validations/request-check/snippet.md url=config.on_prem_url headers=config.headers form_data=config.form_data form_url_encoded_data=config.form_url_encoded_data body_file=config.body_file body=config.body body_cmd=config.body_cmd method=config.method user=config.user sleep=config.sleep inline_sleep=config.inline_sleep display_headers=config.display_headers cookie_jar=config.cookie_jar cookie=config.cookie message=config.message mtls=config.mtls count=config.count insecure=config.insecure expected_headers=config.expected_headers output=config.output capture=config.capture jq=config.jq %}' # rubocop:disable Layout/LineLength
+      )
     end
 
     context 'markdown template' do
       subject(:template_source) { File.read('app/_includes/how-tos/validations/request-check/index.md') }
 
       it 'renders the snippet include for konnect' do
-        expect(template_source).to include('{% include how-tos/validations/request-check/snippet.md url=config.konnect_url')
+        expect(template_source).to include(
+          '{% include how-tos/validations/request-check/snippet.md url=config.konnect_url headers=config.headers form_data=config.form_data form_url_encoded_data=config.form_url_encoded_data body_file=config.body_file body=config.body body_cmd=config.body_cmd method=config.method user=config.user sleep=config.sleep inline_sleep=config.inline_sleep display_headers=config.display_headers cookie_jar=config.cookie_jar cookie=config.cookie message=config.message mtls=config.mtls count=config.count insecure=config.insecure expected_headers=config.expected_headers output=config.output capture=config.capture jq=config.jq %}' # rubocop:disable Layout/LineLength
+        )
       end
 
       it 'renders the snippet include for on-prem' do
-        expect(template_source).to include('{% include how-tos/validations/request-check/snippet.md url=config.on_prem_url')
+        expect(template_source).to include(
+          '{% include how-tos/validations/request-check/snippet.md url=config.on_prem_url headers=config.headers form_data=config.form_data form_url_encoded_data=config.form_url_encoded_data body_file=config.body_file body=config.body body_cmd=config.body_cmd method=config.method user=config.user sleep=config.sleep inline_sleep=config.inline_sleep display_headers=config.display_headers cookie_jar=config.cookie_jar cookie=config.cookie message=config.message mtls=config.mtls count=config.count insecure=config.insecure expected_headers=config.expected_headers output=config.output capture=config.capture jq=config.jq %}' # rubocop:disable Layout/LineLength
+        )
       end
     end
   end
