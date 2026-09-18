5529a7f88a2b6d2f64d9f9669cc18e7bd9378021
Fabian Rodriguez <fabian.rodriguez@konghq.com>
2026-09-18 18:56:08 +0200

    Print config.test_attribute in http_request.html
    Keeps block as the attribute value, only the attribute name now comes
    from the drop, so {% http_request %} accepts section: prereqs/cleanup
    like the other request blocks.

--- spec/app/_plugins/blocks/http_request_spec.rb
+++ spec/app/_plugins/blocks/http_request_spec.rb
@@ -25,6 +25,56 @@ RSpec.describe Jekyll::HttpRequest do
     it 'renders a data-test-step attribute' do
       expect(html).to have_css('div[data-test-step]')
     end
+
+    context 'when section is prereqs' do
+      let(:template) do
+        <<~LIQUID
+          {% http_request %}
+          url: /mock/anything
+          method: GET
+          section: prereqs
+          {% endhttp_request %}
+        LIQUID
+      end
+
+      it 'renders a data-test-prereq attribute instead of data-test-step' do
+        expect(html).to have_css('div[data-test-prereq="block"]')
+        expect(html).not_to have_css('div[data-test-step]')
+      end
+    end
+
+    context 'when section is cleanup' do
+      let(:template) do
+        <<~LIQUID
+          {% http_request %}
+          url: /mock/anything
+          method: GET
+          section: cleanup
+          {% endhttp_request %}
+        LIQUID
+      end
+
+      it 'renders a data-test-cleanup attribute instead of data-test-step' do
+        expect(html).to have_css('div[data-test-cleanup="block"]')
+        expect(html).not_to have_css('div[data-test-step]')
+      end
+    end
+
+    context 'when section is unrecognized' do
+      let(:template) do
+        <<~LIQUID
+          {% http_request %}
+          url: /mock/anything
+          method: GET
+          section: prereq
+          {% endhttp_request %}
+        LIQUID
+      end
+
+      it 'raises an error naming the rejected value' do
+        expect { rendered }.to raise_error(ArgumentError, /prereq/)
+      end
+    end
   end
 
   describe 'markdown output_format' do
